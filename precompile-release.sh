ac50139970755b59dd06d65b0c55c1be3cfa717f
Daan Wijns <dw.daanwijns@gmail.com>
2020-09-16 19:20:19 +0200

    dependency bump + precompile release script

---
+++ precompile-release.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+build_and_copy(){
+    npm run build
+    VERSION=$(jq -r .version package.json)
+    mkdir ../vuetorrent-release
+    git clone git@github.com:WDaan/VueTorrent.git ../vuetorrent-release
+    cd ../vuetorrent-release
+    git checkout latest-release
+    sudo rm -r public
+    cp -r ../vuetorrent/vuetorrent/* ./
+    
+}
+
+cleanup(){
+    cd ..
+    sudo rm -r vuetorrent-release
+}
+
+commit_and_push(){
+    git add --all
+    git commit -m "Release v$VERSION"
+    git push --set-upstream origin latest-release
+}
+
+build_and_copy
+commit_and_push
+cleanup
