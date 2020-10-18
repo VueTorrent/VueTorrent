76f6db7bb40f26a4e2192ecd68fe82097edc68fd
Daan Wijns <dw.daanwijns@gmail.com>
2020-10-18 12:39:23 +0200

    Release v0.4.0

---
+++ public/precache-manifest.37ee3c99907587077bb648a3fcd5518d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97bc346f1816c941f272",
+    "url": "css/app.dbc2c0d8.css"
+  },
+  {
+    "revision": "a0cdcf49d80a275c8124",
+    "url": "css/chunk-vendors.77244652.css"
+  },
+  {
+    "revision": "1bd4dea51f3131c93d768146af975cb1",
+    "url": "index.html"
+  },
+  {
+    "revision": "97bc346f1816c941f272",
+    "url": "js/app.22b347cd.js"
+  },
+  {
+    "revision": "a0cdcf49d80a275c8124",
+    "url": "js/chunk-vendors.939b01b6.js"
+  },
+  {
+    "revision": "dfbf09b47d536d7c433ac7435e26a17a",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
