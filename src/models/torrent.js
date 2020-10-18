7b9130f4ff23377733deaa4bb818ee90c01baf79
muertocaloh <muertocaloh@protonmail.com>
2020-10-18 11:04:52 +0200

    Fix torrent class bug

--- src/models/torrent.js
+++ src/models/torrent.js
@@ -11,7 +11,7 @@ export default class Torrent {
         this.num_leechs = data.num_leechs
         this.num_seeds = data.num_seeds
         this.path = data.path === undefined ? '/downloads' : data.path
-        this.state = this.formatState(data.state)
+        this.state = this.formatState(data)
         // hash is used to identify
         this.hash = data.hash
         // available seeds
@@ -24,10 +24,10 @@ export default class Torrent {
         this.category = data.category
     }
 
-    formatState(state) {
+    formatState(item) {
         if (!item.tracker) return 'Fail'
 
-        switch (state) {
+        switch (item.state) {
         case 'forceDL':
         case 'downloading':
             return 'Downloading'
