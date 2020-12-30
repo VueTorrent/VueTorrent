183031e98ec8a52098b83b91531a7db49b52b8fc
Valerio Coltrè <colthreepv@gmail.com>
2020-12-30 10:30:55 +0100

    mobile devices spacing issues (#87)
    * optimizing space on mobile devices
    
    * navbar fixes for mobile devices
    
    * flex shrink disabled
    
    * fix without extra scss
    
    Co-authored-by: Daan Wijns <dw.daanwijns@gmail.com>

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -1,106 +1,105 @@
 <template>
-  <nav>
-    <!--title-->
-    <v-app-bar app flat color="background">
-      <v-app-bar-nav-icon
-        class="grey--text text--lighten-1"
-        @click.stop="drawer = !drawer"
-      />
-      <v-toolbar-title
-        :class="[
-          'grey--text',
-          { 'subheading ml-0': $vuetify.breakpoint.smAndDown }
-        ]"
-      >
-        <div v-if="!$vuetify.breakpoint.xs">
-          <span class="font-weight-light">Vue</span>
-          <span>Torrent</span>
-        </div>
-      </v-toolbar-title>
-      <v-spacer />
+    <nav class="navbar">
+        <!--title-->
+        <v-app-bar app flat color="background">
+            <v-app-bar-nav-icon
+                @click.stop="drawer = !drawer"
+                class="grey--text text--lighten-1"
+            ></v-app-bar-nav-icon>
+            <v-toolbar-title
+                :class="[
+                    'grey--text',
+                    { 'subheading ml-0': $vuetify.breakpoint.smAndDown }
+                ]"
+            >
+                <div v-if="!$vuetify.breakpoint.xs">
+                    <span class="font-weight-light">Vue</span>
+                    <span>Torrent</span>
+                </div>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
 
-      <TopActions :show-all="webuiSettings.showGlobalRemoveResumePause" />
-    </v-app-bar>
-    <!--navigation drawer itself -->
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-      class="primary drawer"
-      style="position: fixed;"
-      width="280"
-      disable-resize-watcher
-    >
-      <v-flex
-        v-if="status"
-        style="height: 98vh; display: flex; flex-direction: column;"
-        class="mt-3"
-      >
-        <CurrentSpeed
-          v-if="webuiSettings.showCurrentSpeed"
-          :status="status"
-        />
+            <TopActions :showAll="webuiSettings.showGlobalRemoveResumePause" />
+        </v-app-bar>
+        <!--navigation drawer itself -->
+        <v-navigation-drawer
+            app
+            v-model="drawer"
+            class="primary drawer"
+            style="position: fixed;"
+            width="280"
+            disable-resize-watcher
+        >
+            <v-flex style="height: 98vh; display: flex; flex-direction: column;"
+                    class="mt-3" v-if="status">
+                <CurrentSpeed
+                    v-if="webuiSettings.showCurrentSpeed"
+                    :status="status"
+                />
 
-        <SpeedGraph v-if="webuiSettings.showSpeedGraph" />
+                <SpeedGraph v-if="webuiSettings.showSpeedGraph" />
 
-        <div class="mt-4" />
-        <SessionStats
-          v-if="webuiSettings.showSessionStat"
-          :status="status"
-        />
+                <div class="mt-4"></div>
+                <SessionStats
+                    v-if="webuiSettings.showSessionStat"
+                    :status="status"
+                />
 
-        <FreeSpace
-          v-if="webuiSettings.showFreeSpace"
-          :space="status.freeDiskSpace"
-        />
-        <FilterSelect :show-tracker-filter="webuiSettings.showTrackerFilter" />
-        <div class="mt-auto">
-          <BottomActions />
-        </div>
-      </v-flex>
-    </v-navigation-drawer>
-  </nav>
+                <FreeSpace
+                    v-if="webuiSettings.showFreeSpace"
+                    :space="status.freeDiskSpace"
+                />
+                <FilterSelect :showTrackerFilter="webuiSettings.showTrackerFilter" />
+              <div class="mt-auto">
+                <BottomActions />
+              </div>
+            </v-flex>
+        </v-navigation-drawer>
+    </nav>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 import {
-  BottomActions,
-  TopActions,
-  SpeedGraph,
-  FreeSpace,
-  SessionStats,
-  CurrentSpeed,
-  FilterSelect
-} from './index'
-
-export default {
-  name: 'Navbar',
-  components: {
-    FreeSpace,
     BottomActions,
     TopActions,
     SpeedGraph,
+    FreeSpace,
     SessionStats,
     CurrentSpeed,
     FilterSelect
-  },
-  data() {
-    return {
-      drawer: this.$vuetify.breakpoint.mdAndUp
-    }
-  },
-  computed: {
-    ...mapGetters(['getTheme', 'getWebuiSettings', 'getStatus']),
-    webuiSettings() {
-      return this.getWebuiSettings()
+} from './index'
+import './NavBar.scss'
+
+export default {
+    name: 'Navbar',
+    components: {
+        FreeSpace,
+        BottomActions,
+        TopActions,
+        SpeedGraph,
+        SessionStats,
+        CurrentSpeed,
+        FilterSelect
     },
-    status() {
-      return this.getStatus()
+
+    computed: {
+        ...mapGetters(['getTheme', 'getWebuiSettings', 'getStatus']),
+        webuiSettings() {
+            return this.getWebuiSettings()
+        },
+        status() {
+            return this.getStatus()
+        }
+    },
+    data() {
+        return {
+            drawer: this.$vuetify.breakpoint.mdAndUp
+        }
+    },
+    created() {
+        this.$vuetify.theme.dark = this.getTheme()
     }
-  },
-  created() {
-    this.$vuetify.theme.dark = this.getTheme()
-  }
 }
 </script>
 
