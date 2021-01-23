2b84780545472bb0e8dcd4b33f936dd96e8987bd
Daan Wijns <dw.daanwijns@gmail.com>
2021-01-23 13:51:42 +0100

    current speed alignment

--- src/components/Navbar/CurrentSpeed.vue
+++ src/components/Navbar/CurrentSpeed.vue
@@ -3,36 +3,40 @@
     <div class="text-uppercase white--text caption ml-4 font-weight-medium">
       current speed
     </div>
-    <v-card color="secondary" flat class="mr-2 ml-2 text-center">
-      <v-layout row wrap class="py-3 px-1 mt-1 project mx-auto">
-        <v-icon class="mt-2" color="download" size="18px">
-          {{ mdiChevronDown }}
-        </v-icon>
-        <span
-          class="download--text title"
-          style="font-size: 1.1em !important;"
-        >
-          204.34
-          <span class="font-weight-light caption font-weight-regular">
-            {{ status.dlspeed | getDataUnit(1) }}/s
+    <v-card color="secondary" flat class="mr-2 ml-2">
+      <v-layout row wrap class="py-3 px-1 mt-1 project mx-auto text-center">
+        <v-flex xs6>
+          <v-icon color="download" size="16px">
+            {{ mdiChevronDown }}
+          </v-icon>
+          <span
+            class="download--text title"
+            style="font-size: 1.1em !important;"
+          >
+            {{ status.dlspeed | getDataValue(1) }}
+            <span class="font-weight-light caption font-weight-regular">
+              {{ status.dlspeed | getDataUnit(1) }}/s
+            </span>
           </span>
-        </span>
-        <v-icon
-          class="pl-2 mt-1"
-          color="upload"
-          size="24px"
-        >
-          {{ mdiChevronUp }}
-        </v-icon>
-        <span
-          class="upload--text title"
-          style="font-size: 1.1em !important;"
-        >
-          204.34
-          <span class="font-weight-light caption font-weight-regular">
-            {{ status.upspeed | getDataUnit }}/s
+        </v-flex>
+        <v-flex xs6>
+          <v-icon
+            class="pl-2"
+            color="upload"
+            size="24px"
+          >
+            {{ mdiChevronUp }}
+          </v-icon>
+          <span
+            class="upload--text title"
+            style="font-size: 1.1em !important;"
+          >
+            {{ status.upspeed | getDataValue(1) }}
+            <span class="font-weight-light caption font-weight-regular">
+              {{ status.upspeed | getDataUnit }}/s
+            </span>
           </span>
-        </span>
+        </v-flex>
       </v-layout>
     </v-card>
   </div>
