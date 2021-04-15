7f9c9bf46d47b4d7148adf7f53bf1dcf907dffc6
WDaan <dw.daanwijns@gmail.com>
2021-04-15 16:04:57 +0000

    Deploying to latest-release from  @ 38432a535e5d881ca23b06d45bd15f92e7fcef12 🚀

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.dc5fd239ca0a88ed5609c6f8cb0883f5.js"
+  "precache-manifest.03c73d7e1f567ce3a6c51e8fabdfd66d.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vuetorrent"});
