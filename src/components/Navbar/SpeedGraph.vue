444a353c0b079e450bc0fb919601ed2dee39ba95
m4ximuel <54457152+m4ximuel@users.noreply.github.com>
2021-04-19 03:27:06 +0900

    Update SpeedGraph.vue

--- src/components/Navbar/SpeedGraph.vue
+++ src/components/Navbar/SpeedGraph.vue
@@ -1,104 +1,153 @@
 <template>
-  <div class="mt-4">
-    <apexcharts
-      ref="chart"
-      type="line"
-      :options="chartOptions"
-      :series="series"
-    />
+  <div class="mt-4" style="position:relative">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 14 100"
+      preserveAspectRatio="none"
+      class="updlGraph"
+      style="width:256px; height:140px;"
+    >
+      <g class="indexBar">
+        <polygon class="hWndIndex" />
+      </g>
+      <g class="uploadSVG">
+        <polygon class="hWndUpload" />
+      </g>
+      <g class="downloadSVG">
+        <polygon class="hWndDownload" />
+      </g>
+    </svg>
+    <v-slide-x-transition>
+      <div
+        v-if="dataIndex >= 0"
+        style="pointer-events: none; position:absolute; top: 3px; left: 3px;"
+      >
+        <v-card
+          flat
+          class="rounded-0"
+          style="opacity: .5;"
+        >
+          <span class="caption mx-2">
+            {{ 28 - dataIndex * 2 }} seconds ago
+          </span>
+        </v-card>
+        <v-card
+          flat
+          class="pl-1 pr-2 rounded-0"
+          style="opacity: .8;"
+        >
+          <p class="caption download--text ma-0 pa-0">
+            <v-icon class="download--text">
+              {{ mdiChevronDown }}
+            </v-icon>
+            {{ indexDownload }}
+          </p>
+          <p class="caption upload--text ma-0 pa-0">
+            <v-icon class="upload--text">
+              {{ mdiChevronUp }}
+            </v-icon>
+            {{ indexUpload }}
+          </p>
+        </v-card>
+      </div>
+    </v-slide-x-transition>
   </div>
 </template>
 
 <script>
-import VueApexCharts from 'vue-apexcharts'
 import { mapGetters } from 'vuex'
 import { getDataUnit, getDataValue } from '@/filters'
+import { 
+  mdiChevronUp, mdiChevronDown
+} from '@mdi/js'
 
 export default {
   name: 'SpeedGraph',
-  components: {
-    apexcharts: VueApexCharts
-  },
   data() {
     return {
-      chartOptions: {
-        chart: {
-          sparkline: {
-            enabled: true
-          },
-          animations: {
-            enabled: false,
-            dynamicAnimation: {
-              speed: 1000
-            }
-          }
-        },
-        colors: [
-          this.$vuetify.theme.currentTheme.upload,
-          this.$vuetify.theme.currentTheme.download
-        ],
-        stroke: {
-          show: true,
-          curve: 'smooth',
-          lineCap: 'round',
-          width: 4
-        },
-        fill: {
-          type: 'gradient',
-          gradient: {
-            shade: 'dark',
-            type: 'vertical',
-            shadeIntensity: 0.5,
-            opacityFrom: 0.6,
-            opacityTo: 0.5,
-            stops: [0, 50, 100]
-          }
-        },
-        tooltip: {
-          theme: 'light',
-          x: {
-            formatter: value => {
-              const val = 32 - value * 2
-              
-              return val + ' seconds ago'
-            }
-          },
-          y: {
-            formatter: value => {
-              return `${getDataValue(value, 0)} ${getDataUnit(value)}`
-            }
-          }
-        }
-      }
+      mdiChevronUp, mdiChevronDown,
+      hWndDownload: null,
+      hWndUpload: null,
+      hWndIndex: null,
+      indexTimeout: 0,
+      dataIndex: -1
     }
   },
   computed: {
-    series() {
-      return [
-        {
-          name: 'upload',
-          type: 'area',
-          data: this.$store.state.upload_data
-        },
-        {
-          name: 'download',
-          type: 'area',
-          data: this.$store.state.download_data
-        }
-      ]
-    },
     ...mapGetters(['getTheme']),
     theme() {
       return this.getTheme() ? 'Dark' : 'Light'
+    },
+    indexDownload() {
+      console.log(getDataUnit)
+
+      return getDataValue(this.$store.state.download_data[this.dataIndex]) + ' ' +
+        getDataUnit(this.$store.state.download_data[this.dataIndex]) + '/s'
+    },
+    indexUpload() {
+
+      return getDataValue(this.$store.state.upload_data[this.dataIndex]) + ' ' +
+        getDataUnit(this.$store.state.upload_data[this.dataIndex]) + '/s'
     }
   },
   watch: {
     theme(newValue) {
       this.setChartTooltipTheme(newValue)
+    },
+    '$store.state.download_data'(val) {
+      var 
+        max = Math.max.apply(null, val),
+        i, v, d_dat = ''
+      for (i = 0; i < val.length; i++) {
+        v = 100 - val[i] / max * 100
+        d_dat += i + ',' + (Number.isNaN(v) ? 100 : v) + ' '
+      }
+      this.hWndDownload.setAttribute('points', '0,100 ' + d_dat + '14,100')
+    },
+    '$store.state.upload_data'(val) {
+      var 
+        max = Math.max.apply(null, val),
+        i, v, d_dat = ''
+      for (i = 0; i < val.length; i++) {
+        v = 100 - val[i] / max * 100
+        d_dat += i + ',' + (Number.isNaN(v) ? 100 : v) + ' '
+      }
+      this.hWndUpload.setAttribute('points', '0,100 ' + d_dat + '14,100')
     }
   },
   mounted() {
-    this.setChartTooltipTheme(this.theme)
+    this.hWndDownload = this.$el.getElementsByClassName('hWndDownload')[0]
+    this.hWndUpload = this.$el.getElementsByClassName('hWndUpload')[0]
+    this.hWndIndex = this.$el.getElementsByClassName('hWndIndex')[0]
+
+    const svgGraph = this.$el.getElementsByClassName('updlGraph')[0]
+    svgGraph.addEventListener('mousemove', function (event) {
+      const svgWidth = 255
+      const calcIndex = Math.floor(event.layerX / svgWidth * 14)
+      if (this.dataIndex != calcIndex) {
+        this.dataIndex = calcIndex
+        this.hWndIndex.setAttribute('points',
+          calcIndex + ',100 ' + (calcIndex + 1) + ',100 ' + (calcIndex + 1) + ',0 ' + calcIndex + ',0'
+        )
+        this.hWndUpload.setAttribute('points', this.hWndUpload.getAttribute('points'))
+        this.hWndDownload.setAttribute('points', this.hWndDownload.getAttribute('points'))
+        clearTimeout(this.indexTimeout)
+        this.indexTimeout = setTimeout(function () {
+          this.hWndIndex.setAttribute('points', '')
+          this.hWndUpload.setAttribute('points', this.hWndUpload.getAttribute('points'))
+          this.hWndDownload.setAttribute('points', this.hWndDownload.getAttribute('points'))
+          this.dataIndex = -1
+        }.bind(this), 10000)
+      }
+    }.bind(this), false)
+    svgGraph.addEventListener('mouseleave', function (event) {
+      clearTimeout(this.indexTimeout)
+      this.hWndIndex.setAttribute('points', '')
+      this.hWndUpload.setAttribute('points', this.hWndUpload.getAttribute('points'))
+      this.hWndDownload.setAttribute('points', this.hWndDownload.getAttribute('points'))
+      this.dataIndex = -1
+    }.bind(this), false)
+
   },
   methods: {
     setChartTooltipTheme(theme) {
@@ -108,3 +157,20 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+
+.indexBar {
+  fill: var(--v-secondary-base);
+  //opacity: .5;
+}
+.downloadSVG {
+  fill: var(--v-download-base);
+  opacity: .5;
+}
+.uploadSVG {
+  fill: var(--v-upload-base);
+  opacity: .5;
+}
+
+</style>
