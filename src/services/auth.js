0c1e74e8f0c945659220c276c5713a8430db0c5c
rare-magma <nun0@posteo.co>
2021-10-25 20:24:43 +0200

    Revert "fix: disable auth only if already authenticated"
    This reverts commit 6a20c96c5bef0b80ca38a400ba5c6b5314407057.

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,7 @@
 import store from '../store'
 
 export function isAuthenticated() {
-  if (store.state.webuiSettings.disableAuthentication && store.state.authenticated) {
+  if (store.state.webuiSettings.disableAuthentication) {
     return true
   }
 
