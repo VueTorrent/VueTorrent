6a20c96c5bef0b80ca38a400ba5c6b5314407057
rare-magma <nun0@posteo.co>
2021-10-25 19:27:23 +0200

    fix: disable auth only if already authenticated

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,7 @@
 import store from '../store'
 
 export function isAuthenticated() {
-  if (store.state.webuiSettings.disableAuthentication) {
+  if (store.state.webuiSettings.disableAuthentication && store.state.authenticated) {
     return true
   }
 
