4db13e3db07d225a2beffd947095146cf5f489d5
Robert Lee <pboymt@foxmail.com>
2021-11-25 09:58:04 +0800

    add chinese translation

---
+++ src/lang/zh.js
@@ -0,0 +1,25 @@
+const locale = {
+  /** General */
+  category: '分类',
+  settings: '设置',
+
+  /** Navbar */
+  navbar: {
+    currentSpeed: '当前速率',
+    freeSpace: '剩余磁盘空间',
+    topActions: {
+      openSettings: '打开设置',
+      searchNew: '搜索新的Torrent'
+    }
+  },
+
+  /** Modals */
+  modals: {
+    add: {
+      title: '添加新的Torrent',
+      selectFiles: '选择文件'
+    }
+  }
+}
+
+export default locale
