a377010cd5724346d0a7c37fc024823d7d9c8c4a
giacomocerquone <cerquone96@hotmail.it>
2022-04-16 02:31:56 +0200

    feat: add qbits api calls to fetch and set global limits

--- src/services/qbit.js
+++ src/services/qbit.js
@@ -225,6 +225,28 @@ class Qbit {
     return this.torrentAction('setUploadLimit', hashes, { limit })
   }
 
+  getGlobalDownloadLimit() {
+    return this.axios.get('/transfer/downloadLimit')
+  }
+
+  getGlobalUploadLimit() {
+    return this.axios.get('/transfer/uploadLimit')
+  }
+
+  setGlobalDownloadLimit(limit) {
+    const formData = new FormData()
+    formData.append('limit', limit)
+    
+    return this.axios.post('/transfer/setDownloadLimit', formData)
+  }
+
+  setGlobalUploadLimit(limit) {
+    const formData = new FormData()
+    formData.append('limit', limit)
+
+    return this.axios.post('/transfer/setUploadLimit', formData)
+  }
+
   setShareLimit(hashes, ratioLimit, seedingTimeLimit) {
     return this.torrentAction('setShareLimits', hashes, { ratioLimit, seedingTimeLimit })
   }
