a943c8a02cbe2f5333b2b276a1cf0234d88c6556
Larsluph <remarso59@sfr.fr>
2023-01-22 14:24:46 +0100

    Duplicate reference in dashboard settings

--- src/store/index.js
+++ src/store/index.js
@@ -97,8 +97,8 @@ export default new Vuex.Store({
       paginationSize: 15,
       dateFormat: 'DD/MM/YYYY, HH:mm:ss',
       openSideBarOnStart: true,
-      busyTorrentProperties: [...propertiesTemplate],
-      doneTorrentProperties: [...propertiesTemplate]
+      busyTorrentProperties: JSON.parse(JSON.stringify(propertiesTemplate)),
+      doneTorrentProperties: JSON.parse(JSON.stringify(propertiesTemplate))
     },
     categories: [],
     trackers: [],
