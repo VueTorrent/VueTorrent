3c30adc18c7f8efc0e6ab58a50859b652b2375f2
Larsluph <22910497+Larsluph@users.noreply.github.com>
2023-02-18 17:11:39 +0100

    tests: fix tests

--- src/mixins/SettingsTab.ts
+++ src/mixins/SettingsTab.ts
@@ -1,26 +1,28 @@
-import { mapState } from 'vuex'
+import { mapGetters } from 'vuex'
 import { defineComponent } from 'vue'
 import qbit from '@/services/qbit'
 
 export default defineComponent({
   name: 'SettingsTab',
   computed: {
-    ...mapState(['settings'])
+    ...mapGetters(['getSettings']),
+    settings() {
+      return this.getSettings()
+    }
   },
   methods: {
     async saveSettings() {
-      qbit.setPreferences(this.settings).then(() => {
-        this.$toast.success(this.$t('toast.settingsSaved').toString())
-      })
+      await qbit.setPreferences(this.settings)
+      this.$toast.success(this.$t('toast.settingsSaved').toString())
       await this.$store.dispatch('FETCH_SETTINGS')
       await this.$store.commit('SET_LANGUAGE')
       this.close()
+
       if (!this.settings.alternative_webui_enabled) {
-        navigator.serviceWorker.getRegistrations().then(function (registrations) {
-          for (const registration of registrations) {
-            registration.unregister()
-          }
-        })
+        const registrations = await navigator.serviceWorker.getRegistrations()
+        for (const registration of registrations) {
+          await registration.unregister()
+        }
       }
     }
   }
