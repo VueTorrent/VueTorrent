c9914172b55ac1f3e1cdf89a01caec2666f153e3
Rémi Marseault <22910497+Larsluph@users.noreply.github.com>
2023-03-19 16:19:50 +0100

    perf: Progress component sizing on Desktop (#720)

--- src/components/Torrent/DashboardItems/Progress.vue
+++ src/components/Torrent/DashboardItems/Progress.vue
@@ -1,9 +1,9 @@
 <template>
-  <v-flex xs12 sm1 md1 class="mr-4">
+  <v-flex xs12 sm1 md1>
     <div class="caption grey--text">
       {{ $t('torrent.properties.progress') | titleCase }}
     </div>
-    <v-progress-linear :value="torrent.progress" height="20" :style="phoneLayout ? '' : 'width: 80%;'" :color="`torrent-${state}`" rounded>
+    <v-progress-linear :value="torrent.progress" height="20" style="width: 90%" :color="`torrent-${state}`" rounded>
       <span class="caption white--text"> {{ torrent.progress }}% </span>
     </v-progress-linear>
   </v-flex>
