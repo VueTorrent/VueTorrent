f8997b1123fe2dc762ed3463ed9ecf16054c087e
Daan Wijns <dw.daanwijns@gmail.com>
2023-04-12 17:23:14 +0200

    fix: tests

--- src/components/Core/StorageCard.spec.ts
+++ src/components/Core/StorageCard.spec.ts
@@ -2,15 +2,17 @@ import { describe, it, expect } from 'vitest'
 import { setup } from '@/utils/vitest'
 import StorageCard from '@/components/Core/StorageCard.vue'
 
+const label = 'Downloaded'
+const color = 'download'
+
 describe('StorageCard.vue', () => {
   it('should render the label', () => {
-    const label = 'Downloaded'
-    const wrapper = setup(StorageCard, { label })
+    const wrapper = setup(StorageCard, { label, color, value: 0 })
     expect(wrapper.find('[data-testid="StorageCard-label"]').text()).toEqual(label)
   })
 
   it('should render value and unit & be formatted', () => {
-    const wrapper = setup(StorageCard, { value: 10000 })
+    const wrapper = setup(StorageCard, { label, color, value: 10000 })
     expect(wrapper.find('[data-testid="StorageCard-value"]').exists()).toBe(true)
     expect(wrapper.find('[data-testid="StorageCard-value"]').text()).toBe('9.77')
 
@@ -19,11 +21,8 @@ describe('StorageCard.vue', () => {
   })
 
   it('text should have the passed-in color', () => {
-    const color = 'download'
-    const wrapper = setup(StorageCard, { color })
-    expect(wrapper.find('[data-testid="StorageCard-label"]').classes()).toContain(color + '--text')
-    expect(wrapper.find('[data-testid="StorageCard-Wrapper"]').classes()).toContain(
-      color + '--text'
-    )
+    const wrapper = setup(StorageCard, { label, color, value: 1 })
+    expect(wrapper.find('[data-testid="StorageCard-label"]').classes()).toContain('text-' + color)
+    expect(wrapper.find('[data-testid="StorageCard-Wrapper"]').classes()).toContain('text-' + color)
   })
 })
--- src/utils/vitest.ts
+++ src/utils/vitest.ts
@@ -1,12 +1,13 @@
 import { mount } from '@vue/test-utils'
 import { createVuetify } from 'vuetify'
+import VueApexCharts from 'vue3-apexcharts'
 
 export function setup(component: any, props?: any) {
   const vuetify = createVuetify()
 
   return mount(component, {
     global: {
-      plugins: [vuetify, createPinia()]
+      plugins: [vuetify, createPinia(), VueApexCharts]
     },
     props
   })
