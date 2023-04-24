da733d3e2816bd78f3ec1c4b5f097052fb9c6c6b
Nick Jones <nicolasj@mtu.edu>
2023-04-24 13:23:38 -0400

    Remove tooltip from all-time stats section

--- src/components/Navbar/AllTimeStats.vue
+++ src/components/Navbar/AllTimeStats.vue
@@ -3,14 +3,6 @@
     <label class="text-uppercase white--text caption font-weight-medium ml-4">
       {{ $t('navbar.alltimeTitle') }}
     </label>
-    <v-tooltip bottom>
-      <template #activator="{ on }">
-        <v-icon color="white" style="opacity: 0.3" small v-on="on">
-          {{ mdiInformationOutline }}
-        </v-icon>
-      </template>
-      <span>{{ $t('navbar.sessionStats.tooltip') }}</span>
-    </v-tooltip>
     <StorageCard class="mb-4 mt-4" :label="titleCase($t('downloaded'))" color="download" :value="getDownload" />
     <StorageCard class="mb-4 mt-4" :label="titleCase($t('uploaded'))" color="upload" :value="getUpload" />
     <StringCard :label="titleCase($t('ratio'))" color="ratio" :value="getRatio" />
