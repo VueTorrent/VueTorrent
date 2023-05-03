773dcc23ade07fa17d3d7a8e29865e711c92dbe7
Larsluph <22910497+Larsluph@users.noreply.github.com>
2023-05-04 00:22:10 +0200

    perf: Create missing Speed and Advanced tabs

---
+++ src/components/Settings/Tabs/Advanced.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card flat>
+    <!-- TODO -->
+    <v-list-item>
+      Item 1
+    </v-list-item>
+    <v-list-item>
+      Item 2
+    </v-list-item>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { FullScreenModal, SettingsTab } from '@/mixins'
+
+export default defineComponent({
+  name: 'Advanced',
+  mixins: [SettingsTab, FullScreenModal]
+})
+</script>
