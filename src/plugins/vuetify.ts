8bc2bcf44f09d28d0388c35e338043e99c20a773
LeaderbotX400 <eric@singerfamily.ca>
2023-05-30 17:32:31 -0600

    fix: use material 3

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,11 +1,9 @@
 import 'vuetify/styles'
-import { md3 } from 'vuetify/blueprints'
 import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
 import { createVuetify } from 'vuetify'
 import { lightTheme, darkTheme } from '@/themes'
 
 export default createVuetify({
-  blueprint: md3,
   icons: {
     defaultSet: 'mdi',
     aliases,
