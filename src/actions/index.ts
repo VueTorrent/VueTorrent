189bb27eef66e4b363caeb25c95b6c60fd5323ee
Larsluph <22910497+Larsluph@users.noreply.github.com>
2023-07-13 19:46:08 +0200

    fix(tags): Fix tags being duplicated

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,7 +1,6 @@
 import { DocumentTitle } from './DocumentTitle'
-import { Tags } from './Tags'
 import { Torrents } from './Torrents'
 import { Trackers } from './Trackers'
 import { Graph } from './Graph'
 
-export { DocumentTitle, Tags, Torrents, Trackers, Graph }
+export { DocumentTitle, Torrents, Trackers, Graph }
