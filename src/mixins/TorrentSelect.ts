1ba088163cc88ebd9917086995c911605c57b57a
Rémi Marseault <22910497+Larsluph@users.noreply.github.com>
2023-07-16 22:03:21 +0200

    perf(Torrent Card): Highlight torrent card when TRC menu is showed (#966)

--- src/mixins/TorrentSelect.ts
+++ src/mixins/TorrentSelect.ts
@@ -1,20 +1,27 @@
-import { Component, Vue } from 'vue-property-decorator'
+import { defineComponent } from 'vue'
+import { mapGetters, mapState } from 'vuex'
 
-@Component
-export default class TorrentSelect extends Vue {
-  isAlreadySelected(hash: string) {
-    return this.$store.getters.containsTorrent(hash)
-  }
-  selectTorrent(hash: string) {
-    if (!this.$store.state.selectMode) this.$store.state.selectMode = true
-    if (this.isAlreadySelected(hash)) {
-      this.$store.commit('SET_SELECTED', { type: 'remove', hash })
-    } else {
-      this.$store.commit('SET_SELECTED', { type: 'add', hash })
+export default defineComponent({
+  name: 'TorrentSelect',
+  computed: {
+    ...mapState(['selectMode']),
+    ...mapGetters(['containsTorrent'])
+  },
+  methods: {
+    isAlreadySelected(hash: string) {
+      return this.containsTorrent(hash)
+    },
+    selectTorrent(hash: string) {
+      if (!this.selectMode) this.selectMode = true
+      if (this.isAlreadySelected(hash)) {
+        this.$store.commit('SET_SELECTED', {type: 'remove', hash})
+      } else {
+        this.$store.commit('SET_SELECTED', {type: 'add', hash})
+      }
+    },
+    selectUntil(hash: string, index: number) {
+      if (!this.selectMode) return
+      this.$store.commit('SET_SELECTED', {type: 'until', hash, index})
     }
   }
-  selectUntil(hash: string, index: number) {
-    if (!this.$store.state.selectMode) return
-    this.$store.commit('SET_SELECTED', { type: 'until', hash, index })
-  }
-}
+})
