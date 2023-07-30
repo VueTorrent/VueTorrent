2d583d9559c32d95ac4b619d8985e12b3eba428d
Larsluph <22910497+Larsluph@users.noreply.github.com>
2023-07-30 14:48:36 +0200

    Setup project with vue3 + vite + Vuetify

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <v-card>
+    <v-card-title>
+      Card title
+    </v-card-title>
+    <v-card-text>
+      Card text
+    </v-card-text>
+  </v-card>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives
+})
+
+createApp(App)
+  .use(vuetify)
+  .mount('#app')
