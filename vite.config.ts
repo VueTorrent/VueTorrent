e285f29c4ba9fcddce31c42fd9bf0ff6b8f77d59
Larsluph <22910497+Larsluph@users.noreply.github.com>
2023-07-30 14:51:40 +0200

    Fix path aliases not working

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
   build: {
-    outDir: 'vuetorrent'
+    target: 'esnext',
+    outDir: './vuetorrent/public'
+  },
+  define: {
+    'process.env': {}
+  },
+  plugins: [
+    vue()
+  ],
+  publicDir: './public',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   }
 })
