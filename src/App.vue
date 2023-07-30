5064380fcac1fecb3f2b5e5943aceb747709358f
Larsluph <22910497+Larsluph@users.noreply.github.com>
2023-07-30 14:49:27 +0200

    Add mdi icons support

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
 <template>
   <v-card>
     <v-card-title>
+      <v-icon icon="mdi-home" color="red" />
       Card title
     </v-card-title>
     <v-card-text>
@@ -13,16 +14,4 @@
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,14 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
 
 const vuetify = createVuetify({
   components,
-  directives
+  directives,
+  icons: {
+    defaultSet: 'mdi'
+  }
 })
 
 createApp(App)
