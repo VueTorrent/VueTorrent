04882333f764787a7cfd182077d8cd427b1f7300
Larsluph <22910497+Larsluph@users.noreply.github.com>
2023-08-02 14:53:31 +0200

    Add categories store

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,5 +1,6 @@
 import { useAppStore } from './app'
 import { useAuthStore } from './auth'
+import {useCategoryStore} from './categories'
 import { useModalStore } from './modals'
 import { usePreferenceStore } from './preferences'
 import { useVueTorrentStore } from './vuetorrent'
@@ -7,6 +8,7 @@ import { useVueTorrentStore } from './vuetorrent'
 export {
   useAppStore,
   useAuthStore,
+  useCategoryStore,
   useModalStore,
   usePreferenceStore,
   useVueTorrentStore
