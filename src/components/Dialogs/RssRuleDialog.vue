94e34164f49ab32bc07b41cc139dcbfa7d1baa13
Larsluph <22910497+Larsluph@users.noreply.github.com>
2023-08-14 19:44:53 +0200

    Add affected feeds + matching articles

--- src/components/Dialogs/RssRuleDialog.vue
+++ src/components/Dialogs/RssRuleDialog.vue
@@ -2,7 +2,7 @@
 import { ContentLayout } from '@/constants/qbit/AppPreferences.ts'
 import { useMaindataStore, useRssStore } from '@/stores'
 import { FeedRule } from '@/types/qbit/models'
-import { computed, onBeforeMount, reactive, ref, watch } from 'vue'
+import { computed, onBeforeMount, onMounted, reactive, ref, watch } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { VForm } from 'vuetify/components'
 
@@ -32,7 +32,7 @@ const isFormValid = ref(false)
 const formData = reactive({
   addPaused: null,
   affectedFeeds: [] as string[],
-  assignedCategory: null,
+  assignedCategory: '',
   enabled: true,
   episodeFilter: '',
   ignoreDays: 0,
@@ -61,7 +61,7 @@ const contentLayoutOptions = [
 ]
 const categories = computed(() => {
   return [
-    { title: t('common.none'), value: null },
+    { title: t('common.none'), value: '' },
     ...maindataStore.categories.map((category) => ({ title: category.name, value: category.name }))
   ]
 })
@@ -107,9 +107,6 @@ async function setRule() {
   lastSavedName.value = formData.name
   await rssStore.fetchRules()
   await updateArticles()
-
-  form.value?.reset()
-  close()
 }
 
 async function selectNone() {
@@ -130,68 +127,102 @@ onBeforeMount(async () => {
   if (hasInitialRule.value) {
     lastSavedName.value = props.initialRule!.name!
     Object.assign(formData, props.initialRule!)
+  } else {
+    form.value?.reset()
   }
+
+  await updateArticles()
 })
 
 watch(() => dialogVisible.value, (value) => emit('update:modelValue', value))
 </script>
 
 <template>
-  <v-dialog v-model="dialogVisible" activator="parent">
+  <v-dialog v-model="dialogVisible" scrollable activator="parent">
     <v-card>
-      <v-card-title>{{ $t(`dialogs.rss.rule.title.${initialRule ? 'edit' : 'create'}`) }}</v-card-title>
+      <v-card-title>
+        <v-toolbar color="transparent" :title="$t(`dialogs.rss.rule.title.${initialRule ? 'edit' : 'create'}`)">
+          <v-btn icon="mdi-close" @click="close" />
+        </v-toolbar>
+      </v-card-title>
       <v-card-text>
         <v-form v-model="isFormValid" ref="form" @submit.prevent>
-          <v-text-field v-model="formData.name"
-                        autofocus
-                        required
-                        :label="$t('dialogs.rss.rule.name')" />
+          <v-row>
+            <v-col cols="12" sm="6" class="overflow-y-auto">
+              <v-text-field v-model="formData.name"
+                            autofocus
+                            required
+                            :label="$t('dialogs.rss.rule.name')" />
 
-          <v-divider />
+              <v-divider />
 
-          <v-checkbox v-model="formData.useRegex"
-                      hide-details
-                      :label="$t('dialogs.rss.rule.useRegex')" />
-          <v-text-field v-model="formData.mustContain"
-                        :label="$t('dialogs.rss.rule.mustContain')" />
-          <v-text-field v-model="formData.mustNotContain"
-                        :label="$t('dialogs.rss.rule.mustNotContain')" />
-          <v-checkbox v-model="formData.smartFilter"
-                      hide-details
-                      :label="$t('dialogs.rss.rule.smartFilter')" />
-          <v-text-field v-model="formData.episodeFilter"
-                        :placeholder="$t('dialogs.rss.rule.episodeFilterPlaceholder')"
-                        :label="$t('dialogs.rss.rule.episodeFilter')" />
+              <v-checkbox v-model="formData.useRegex"
+                          hide-details
+                          :label="$t('dialogs.rss.rule.useRegex')" />
+              <v-text-field v-model="formData.mustContain"
+                            :label="$t('dialogs.rss.rule.mustContain')" />
+              <v-text-field v-model="formData.mustNotContain"
+                            :label="$t('dialogs.rss.rule.mustNotContain')" />
+              <v-checkbox v-model="formData.smartFilter"
+                          hide-details
+                          :label="$t('dialogs.rss.rule.smartFilter')" />
+              <v-text-field v-model="formData.episodeFilter"
+                            :placeholder="$t('dialogs.rss.rule.episodeFilterPlaceholder')"
+                            :label="$t('dialogs.rss.rule.episodeFilter')" />
 
-          <v-divider class="mb-4" />
+              <v-divider class="mb-4" />
 
-          <v-select v-model="formData.assignedCategory"
-                    :items="categories"
-                    :label="$t('dialogs.rss.rule.assignedCategory')" />
-          <v-text-field v-model="formData.savePath"
-                        :placeholder="$t('dialogs.rss.rule.savePathPlaceholder')"
-                        :label="$t('dialogs.rss.rule.savePath')" />
-          <v-text-field v-model="formData.ignoreDays"
-                        type="number"
-                        :hint="$t('dialogs.rss.rule.ignoreDaysHint')"
-                        :label="$t('dialogs.rss.rule.ignoreDays')" />
-          <v-text-field v-model="lastMatch"
-                        disabled
-                        :label="$t('dialogs.rss.rule.lastMatch.label')" />
+              <v-select v-model="formData.assignedCategory"
+                        :items="categories"
+                        :label="$t('dialogs.rss.rule.assignedCategory')" />
+              <v-text-field v-model="formData.savePath"
+                            :placeholder="$t('dialogs.rss.rule.savePathPlaceholder')"
+                            :label="$t('dialogs.rss.rule.savePath')" />
+              <v-text-field v-model="formData.ignoreDays"
+                            type="number"
+                            :hint="$t('dialogs.rss.rule.ignoreDaysHint')"
+                            :label="$t('dialogs.rss.rule.ignoreDays')" />
+              <v-text-field v-model="lastMatch"
+                            disabled
+                            :label="$t('dialogs.rss.rule.lastMatch.label')" />
 
-          <v-divider />
+              <v-divider />
 
-          <v-select v-model="formData.addPaused"
-                    :items="addPausedOptions"
-                    :label="$t('constants.addPaused.title')" />
-          <v-select v-model="formData.torrentContentLayout"
-                    :items="contentLayoutOptions"
-                    :label="$t('constants.contentLayout.title')" />
+              <v-select v-model="formData.addPaused"
+                        :items="addPausedOptions"
+                        :label="$t('constants.addPaused.title')" />
+              <v-select v-model="formData.torrentContentLayout"
+                        :items="contentLayoutOptions"
+                        :label="$t('constants.contentLayout.title')" />
+
+              <v-list-subheader>{{ $t('dialogs.rss.rule.affectedFeedsSubheader') }}</v-list-subheader>
+
+              <v-checkbox v-for="item in rssStore.feeds"
+                          v-model="formData.affectedFeeds"
+                          multiple
+                          hide-details
+                          :label="item.name"
+                          :value="item.url" />
+            </v-col>
+            <v-divider :vertical="$vuetify.display.smAndUp" />
+            <v-col cols="12" sm="6" class="overflow-y-auto">
+              <div class="v-card-title pa-0">{{ $t('dialogs.rss.rule.matchingArticles.title') }}</div>
+              <v-list>
+                <template v-for="item in matchingArticles">
+                  <v-divider v-if="item.type === 'divider'" />
+                  <v-list-subheader inset v-else-if="item.type === 'subheader'">{{ item.value }}</v-list-subheader>
+                  <v-list-item v-else class="mb-3">{{ item.value }}</v-list-item>
+                </template>
+                <v-list-item v-if="matchingArticles.length === 0"
+                             :title="$t('dialogs.rss.rule.matchingArticles.noMatch')" />
+              </v-list>
+            </v-col>
+          </v-row>
         </v-form>
       </v-card-text>
       <v-card-actions>
         <v-spacer />
-        <v-btn color="error" @click="close">{{ $t('common.cancel') }}</v-btn>
+        <v-btn color="error" @click="close">{{ $t('common.close') }}</v-btn>
         <v-btn color="accent" @click="setRule">{{ $t('common.save') }}</v-btn>
       </v-card-actions>
     </v-card>
