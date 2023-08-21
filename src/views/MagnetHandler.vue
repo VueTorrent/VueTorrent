VueTorrent/VueTorrent

commit c57da2d95a1100e43bdeb0d7f62512b88d27af10

fix: magnet handling (#1041)

src/views/MagnetHandler.vue
@@ -7,9 +7,9 @@ import { General } from '@/mixins'
 export default {
   name: 'MagnetHandler',
   mixins: [General],
-  props: ['magnet'],
   created() {
-    this.createModal('AddModal', { initialMagnet: this.magnet })
+    const regex = new RegExp('^\/download\=(.+?)(?:\/(?=$))?$', 'is')
+    this.createModal('AddModal', { initialMagnet: regex.exec(this.$route.fullPath)[1] })
     this.$router.push({ name: 'dashboard' })
   }
 }

src/router.ts
@@ -41,8 +41,7 @@ const router = new Router({
     {
       path: '/download=:magnet',
       name: 'MagnetHandler',
-      component: () => import('./views/MagnetHandler.vue'),
-      props: true
+      component: () => import('./views/MagnetHandler.vue')
     },
     {
       path: '/login',

commit 6432dd94168e37b8af2cd66241234cdec8c7738d

fix: magnet handling issue caused by #1041 (#1056)

src/views/MagnetHandler.vue
@@ -7,9 +7,19 @@ import { General } from '@/mixins'
 export default {
   name: 'MagnetHandler',
   mixins: [General],
+  methods: {
+    decodeMagnet(url) {
+      if (url.startsWith('magnet:?')) {
+        return url
+      } else {
+        return this.decodeMagnet(decodeURIComponent(url))
+      }
+    }
+  },
   created() {
     const regex = new RegExp('^\/download\=(.+?)(?:\/(?=$))?$', 'is')
-    this.createModal('AddModal', { initialMagnet: regex.exec(this.$route.fullPath)[1] })
+    let magnetLink = this.decodeMagnet(regex.exec(this.$route.fullPath)[1])
+    this.createModal('AddModal', { initialMagnet: magnetLink })
     this.$router.push({ name: 'dashboard' })
   }
 }