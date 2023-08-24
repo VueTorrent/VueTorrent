23ea1ece2fc4a12fa2afe74dd919072cc3728165
Larsluph <22910497+Larsluph@users.noreply.github.com>
2023-08-24 22:06:34 +0200

    Add API call to AddTorrentDialog

--- src/components/Dialogs/AddTorrentDialog.vue
+++ src/components/Dialogs/AddTorrentDialog.vue
@@ -124,23 +124,22 @@ watch(() => navbarStore.addTorrentDialogVisible, async (isVisible) => {
       </v-card-title>
 
       <v-card-text>
-        <v-form v-model="isFormValid" @submit.prevent="submit">
-          <v-row>
-            <v-col>
-              <v-file-input v-model="navbarStore.addTorrentDialogFiles"
-                            accept=".torrent"
-                            counter
-                            :show-size="vueTorrentStore.useBinarySize ? 1024 : 1000"
-                            multiple
-                            persistent-clear
-                            persistent-hint
-                            prepend-icon=""
-                            variant="outlined"
-                            :label="t('dialogs.add.files')">
-                <template v-slot:prepend>
-                  <v-icon color="accent">mdi-paperclip</v-icon>
-                </template>
-                <template v-slot:selection="{ fileNames }">
+        <v-row>
+          <v-col>
+            <v-file-input v-model="navbarStore.addTorrentDialogFiles"
+                          accept=".torrent"
+                          counter
+                          :show-size="vueTorrentStore.useBinarySize ? 1024 : 1000"
+                          multiple
+                          persistent-clear
+                          persistent-hint
+                          prepend-icon=""
+                          variant="outlined"
+                          :label="t('dialogs.add.files')">
+              <template v-slot:prepend>
+                <v-icon color="accent">mdi-paperclip</v-icon>
+              </template>
+              <template v-slot:selection="{ fileNames }">
                   <span v-for="(fileName, index) in fileNames"
                         :key="fileName">
                     <v-chip v-if="index < 2" color="accent">{{ fileName }}</v-chip>
@@ -148,142 +147,141 @@ watch(() => navbarStore.addTorrentDialogVisible, async (isVisible) => {
                       {{ t('dialogs.add.fileOverflow', fileNames.length - 2) }}
                     </span>
                   </span>
-                </template>
-              </v-file-input>
-              <v-textarea v-model="navbarStore.addTorrentDialogUrls"
-                          clearable
-                          :label="t('dialogs.add.links')">
-                <template v-slot:prepend>
-                  <v-icon color="accent">mdi-link</v-icon>
-                </template>
-              </v-textarea>
-            </v-col>
-          </v-row>
+              </template>
+            </v-file-input>
+            <v-textarea v-model="navbarStore.addTorrentDialogUrls"
+                        clearable
+                        :label="t('dialogs.add.links')">
+              <template v-slot:prepend>
+                <v-icon color="accent">mdi-link</v-icon>
+              </template>
+            </v-textarea>
+          </v-col>
+        </v-row>
 
-          <v-row>
-            <v-col>
-              <v-combobox v-model="formData.tags"
-                          v-model:search="tagSearch"
-                          :items="maindataStore.tags"
-                          :hide-no-data="false"
-                          clearable
-                          chips
-                          multiple
-                          :label="t('dialogs.add.tags')">
-                <template v-slot:prepend>
-                  <v-icon color="accent">mdi-tag</v-icon>
-                </template>
-                <template v-slot:no-data>
-                  <v-list-item>
-                    <v-list-item-title v-if="tagSearch?.length > 0">
-                      {{ t('dialogs.add.noTagMatch', { query: tagSearch }) }}
-                    </v-list-item-title>
-                    <v-list-item-title v-else>
-                      {{ t('dialogs.add.noTags') }}
-                    </v-list-item-title>
-                  </v-list-item>
-                </template>
-              </v-combobox>
-              <v-combobox v-model="formData.category"
-                          v-model:search="categorySearch"
-                          :items="maindataStore.categories"
-                          item-title="name"
-                          return-object
-                          :hide-no-data="false"
-                          clearable
-                          chips
-                          label="Category"
-                          @update:modelValue="onCategoryChanged">
-                <template v-slot:prepend>
-                  <v-icon color="accent">mdi-label</v-icon>
-                </template>
-                <template v-slot:no-data>
-                  <v-list-item>
-                    <v-list-item-title v-if="categorySearch?.length > 0">
-                      {{ t('dialogs.add.noCategoryMatch', { query: categorySearch }) }}
-                    </v-list-item-title>
-                    <v-list-item-title v-else>
-                      {{ t('dialogs.add.noCategories') }}
-                    </v-list-item-title>
-                  </v-list-item>
-                </template>
-              </v-combobox>
-            </v-col>
-          </v-row>
+        <v-row>
+          <v-col>
+            <v-combobox v-model="formData.tags"
+                        v-model:search="tagSearch"
+                        :items="maindataStore.tags"
+                        :hide-no-data="false"
+                        clearable
+                        chips
+                        multiple
+                        :label="t('dialogs.add.tags')">
+              <template v-slot:prepend>
+                <v-icon color="accent">mdi-tag</v-icon>
+              </template>
+              <template v-slot:no-data>
+                <v-list-item>
+                  <v-list-item-title v-if="tagSearch?.length > 0">
+                    {{ t('dialogs.add.noTagMatch', { query: tagSearch }) }}
+                  </v-list-item-title>
+                  <v-list-item-title v-else>
+                    {{ t('dialogs.add.noTags') }}
+                  </v-list-item-title>
+                </v-list-item>
+              </template>
+            </v-combobox>
+            <v-combobox v-model="formData.category"
+                        v-model:search="categorySearch"
+                        :items="maindataStore.categories"
+                        item-title="name"
+                        return-object
+                        :hide-no-data="false"
+                        clearable
+                        chips
+                        label="Category"
+                        @update:modelValue="onCategoryChanged">
+              <template v-slot:prepend>
+                <v-icon color="accent">mdi-label</v-icon>
+              </template>
+              <template v-slot:no-data>
+                <v-list-item>
+                  <v-list-item-title v-if="categorySearch?.length > 0">
+                    {{ t('dialogs.add.noCategoryMatch', { query: categorySearch }) }}
+                  </v-list-item-title>
+                  <v-list-item-title v-else>
+                    {{ t('dialogs.add.noCategories') }}
+                  </v-list-item-title>
+                </v-list-item>
+              </template>
+            </v-combobox>
+          </v-col>
+        </v-row>
 
-          <v-row>
-            <v-col>
-              <v-text-field v-model="formData.savepath"
-                            :disabled="formData.autoTMM"
-                            :label="t('dialogs.add.savePath')">
-                <template v-slot:prepend>
-                  <v-icon color="accent">mdi-folder</v-icon>
-                </template>
-              </v-text-field>
-            </v-col>
-          </v-row>
+        <v-row>
+          <v-col>
+            <v-text-field v-model="formData.savepath"
+                          :disabled="formData.autoTMM"
+                          :label="t('dialogs.add.savePath')">
+              <template v-slot:prepend>
+                <v-icon color="accent">mdi-folder</v-icon>
+              </template>
+            </v-text-field>
+          </v-col>
+        </v-row>
 
-          <v-row>
-            <v-col>
-              <v-select v-model="formData.contentLayout"
-                        :items="contentLayoutOptions"
-                        hide-details
-                        color="accent"
-                        variant="solo-filled"
-                        rounded="xl"
-                        :label="t('constants.contentLayout.title')" />
-            </v-col>
-          </v-row>
+        <v-row>
+          <v-col>
+            <v-select v-model="formData.contentLayout"
+                      :items="contentLayoutOptions"
+                      hide-details
+                      color="accent"
+                      variant="solo-filled"
+                      rounded="xl"
+                      :label="t('constants.contentLayout.title')" />
+          </v-col>
+        </v-row>
 
-          <v-row>
-            <v-col>
-              <v-select v-model="formData.stopCondition"
-                        :items="stopConditionOptions"
-                        hide-details
-                        color="accent"
-                        variant="solo-filled"
-                        rounded="xl"
-                        :label="t('constants.stopCondition.title')" />
-            </v-col>
-          </v-row>
+        <v-row>
+          <v-col>
+            <v-select v-model="formData.stopCondition"
+                      :items="stopConditionOptions"
+                      hide-details
+                      color="accent"
+                      variant="solo-filled"
+                      rounded="xl"
+                      :label="t('constants.stopCondition.title')" />
+          </v-col>
+        </v-row>
 
-          <v-row>
-            <v-col>
-              <v-list>
-                <v-list-item>
-                  <v-checkbox v-model="formData.startNow"
-                              density="compact"
-                              hide-details
-                              :label="t('dialogs.add.startNow')" />
-                </v-list-item>
-                <v-list-item>
-                  <v-checkbox v-model="formData.skipChecking"
-                              density="compact"
-                              hide-details
-                              :label="t('dialogs.add.skipChecking')" />
-                </v-list-item>
-                <v-list-item>
-                  <v-checkbox v-model="formData.autoTMM"
-                              density="compact"
-                              hide-details
-                              :label="t('dialogs.add.autoTMM')" />
-                </v-list-item>
-                <v-list-item>
-                  <v-checkbox v-model="formData.sequentialDownload"
-                              density="compact"
-                              hide-details
-                              :label="t('dialogs.add.sequentialDownload')" />
-                </v-list-item>
-                <v-list-item>
-                  <v-checkbox v-model="formData.firstLastPiecePrio"
-                              density="compact"
-                              hide-details
-                              :label="t('dialogs.add.firstLastPiecePrio')" />
-                </v-list-item>
-              </v-list>
-            </v-col>
-          </v-row>
-        </v-form>
+        <v-row>
+          <v-col>
+            <v-list>
+              <v-list-item>
+                <v-checkbox v-model="formData.startNow"
+                            density="compact"
+                            hide-details
+                            :label="t('dialogs.add.startNow')" />
+              </v-list-item>
+              <v-list-item>
+                <v-checkbox v-model="formData.skipChecking"
+                            density="compact"
+                            hide-details
+                            :label="t('dialogs.add.skipChecking')" />
+              </v-list-item>
+              <v-list-item>
+                <v-checkbox v-model="formData.autoTMM"
+                            density="compact"
+                            hide-details
+                            :label="t('dialogs.add.autoTMM')" />
+              </v-list-item>
+              <v-list-item>
+                <v-checkbox v-model="formData.sequentialDownload"
+                            density="compact"
+                            hide-details
+                            :label="t('dialogs.add.sequentialDownload')" />
+              </v-list-item>
+              <v-list-item>
+                <v-checkbox v-model="formData.firstLastPiecePrio"
+                            density="compact"
+                            hide-details
+                            :label="t('dialogs.add.firstLastPiecePrio')" />
+              </v-list-item>
+            </v-list>
+          </v-col>
+        </v-row>
       </v-card-text>
 
       <v-card-actions>
