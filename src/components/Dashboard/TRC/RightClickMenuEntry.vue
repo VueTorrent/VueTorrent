3d1d3f65d472fcb9100751c3f59bf69f3a2e6fbb
Larsluph <22910497+Larsluph@users.noreply.github.com>
2023-09-19 21:26:25 +0200

    Fix TRC submenus not working on mobile

--- src/components/Dashboard/TRC/RightClickMenuEntry.vue
+++ src/components/Dashboard/TRC/RightClickMenuEntry.vue
@@ -24,7 +24,7 @@ defineProps<{
       <v-spacer />
       <v-icon v-if="children">mdi-chevron-right</v-icon>
     </div>
-    <v-menu v-if="children" activator="parent" :open-on-hover="true" close-delay="0" open-delay="0" location="right">
+    <v-menu v-if="children" activator="parent" :open-on-hover="true" :open-on-click="true" close-delay="0" open-delay="0" location="right">
       <v-list>
         <RightClickMenuEntry v-for="child in children" v-bind="child" />
       </v-list>
