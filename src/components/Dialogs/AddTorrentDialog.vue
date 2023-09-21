b7be264ff0264139f56eb6849dc3e27350c8db3f
Larsluph <22910497+Larsluph@users.noreply.github.com>
2023-09-21 10:43:02 +0200

    Update chips for AddTorrentDialog

--- src/components/Dialogs/AddTorrentDialog.vue
+++ src/components/Dialogs/AddTorrentDialog.vue
@@ -129,28 +129,31 @@ watch(
       <v-card-text>
         <v-row>
           <v-col>
-            <v-file-input
-              v-model="navbarStore.addTorrentDialogFiles"
-              accept=".torrent"
-              counter
-              :show-size="vueTorrentStore.useBinarySize ? 1024 : 1000"
-              multiple
-              persistent-clear
-              persistent-hint
-              prepend-icon=""
-              variant="outlined"
-              :label="t('dialogs.add.files')"
-            >
+            <v-file-input v-model="navbarStore.addTorrentDialogFiles"
+                          accept=".torrent"
+                          counter
+                          :show-size="vueTorrentStore.useBinarySize ? 1024 : 1000"
+                          multiple
+                          persistent-clear
+                          persistent-hint
+                          prepend-icon=""
+                          variant="outlined"
+                          :label="t('dialogs.add.files')">
               <template v-slot:prepend>
                 <v-icon color="accent">mdi-paperclip</v-icon>
               </template>
               <template v-slot:selection="{ fileNames }">
-                <span v-for="(fileName, index) in fileNames" :key="fileName">
-                  <v-chip v-if="index < fileOverflowDisplayLimit" color="accent">{{ fileName }}</v-chip>
-                  <span v-else-if="index == fileOverflowDisplayLimit" class="text-overline text-grey-darken-2 ml-2">
+                <template v-for="(filename, index) in fileNames">
+                  <v-chip v-if="index < fileOverflowDisplayLimit"
+                          color="accent"
+                          label
+                          size="small"
+                          class="mr-2">{{ filename }}
+                  </v-chip>
+                </template>
+                <span v-if="fileNames.length == fileOverflowDisplayLimit" class="text-overline text-grey-darken-2 ml-2">
                     {{ t('dialogs.add.fileOverflow', fileNames.length - fileOverflowDisplayLimit) }}
                   </span>
-                </span>
               </template>
             </v-file-input>
             <v-textarea v-model="navbarStore.addTorrentDialogUrls" clearable :label="t('dialogs.add.links')">
@@ -163,16 +166,14 @@ watch(
 
         <v-row>
           <v-col>
-            <v-combobox
-              v-model="formData.tags"
-              v-model:search="tagSearch"
-              :items="maindataStore.tags"
-              :hide-no-data="false"
-              clearable
-              chips
-              multiple
-              :label="t('dialogs.add.tags')"
-            >
+            <v-combobox v-model="formData.tags"
+                        v-model:search="tagSearch"
+                        :items="maindataStore.tags"
+                        :hide-no-data="false"
+                        clearable
+                        chips
+                        multiple
+                        :label="t('dialogs.add.tags')">
               <template v-slot:prepend>
                 <v-icon color="accent">mdi-tag</v-icon>
               </template>
@@ -187,18 +188,16 @@ watch(
                 </v-list-item>
               </template>
             </v-combobox>
-            <v-combobox
-              v-model="formData.category"
-              v-model:search="categorySearch"
-              :items="maindataStore.categories"
-              item-title="name"
-              return-object
-              :hide-no-data="false"
-              clearable
-              chips
-              label="Category"
-              @update:modelValue="onCategoryChanged"
-            >
+            <v-combobox v-model="formData.category"
+                        v-model:search="categorySearch"
+                        :items="maindataStore.categories"
+                        item-title="name"
+                        return-object
+                        :hide-no-data="false"
+                        clearable
+                        chips
+                        label="Category"
+                        @update:modelValue="onCategoryChanged">
               <template v-slot:prepend>
                 <v-icon color="accent">mdi-label</v-icon>
               </template>
@@ -228,29 +227,25 @@ watch(
 
         <v-row>
           <v-col>
-            <v-select
-              v-model="formData.contentLayout"
-              :items="contentLayoutOptions"
-              hide-details
-              color="accent"
-              variant="solo-filled"
-              rounded="xl"
-              :label="t('constants.contentLayout.title')"
-            />
+            <v-select v-model="formData.contentLayout"
+                      :items="contentLayoutOptions"
+                      hide-details
+                      color="accent"
+                      variant="solo-filled"
+                      rounded="xl"
+                      :label="t('constants.contentLayout.title')" />
           </v-col>
         </v-row>
 
         <v-row>
           <v-col>
-            <v-select
-              v-model="formData.stopCondition"
-              :items="stopConditionOptions"
-              hide-details
-              color="accent"
-              variant="solo-filled"
-              rounded="xl"
-              :label="t('constants.stopCondition.title')"
-            />
+            <v-select v-model="formData.stopCondition"
+                      :items="stopConditionOptions"
+                      hide-details
+                      color="accent"
+                      variant="solo-filled"
+                      rounded="xl"
+                      :label="t('constants.stopCondition.title')" />
           </v-col>
         </v-row>
 
@@ -258,19 +253,34 @@ watch(
           <v-col>
             <v-list>
               <v-list-item>
-                <v-checkbox v-model="formData.startNow" density="compact" hide-details :label="t('dialogs.add.startNow')" />
+                <v-checkbox v-model="formData.startNow"
+                            density="compact"
+                            hide-details
+                            :label="t('dialogs.add.startNow')" />
               </v-list-item>
               <v-list-item>
-                <v-checkbox v-model="formData.skipChecking" density="compact" hide-details :label="t('dialogs.add.skipChecking')" />
+                <v-checkbox v-model="formData.skipChecking"
+                            density="compact"
+                            hide-details
+                            :label="t('dialogs.add.skipChecking')" />
               </v-list-item>
               <v-list-item>
-                <v-checkbox v-model="formData.autoTMM" density="compact" hide-details :label="t('dialogs.add.autoTMM')" />
+                <v-checkbox v-model="formData.autoTMM"
+                            density="compact"
+                            hide-details
+                            :label="t('dialogs.add.autoTMM')" />
               </v-list-item>
               <v-list-item>
-                <v-checkbox v-model="formData.sequentialDownload" density="compact" hide-details :label="t('dialogs.add.sequentialDownload')" />
+                <v-checkbox v-model="formData.sequentialDownload"
+                            density="compact"
+                            hide-details
+                            :label="t('dialogs.add.sequentialDownload')" />
               </v-list-item>
               <v-list-item>
-                <v-checkbox v-model="formData.firstLastPiecePrio" density="compact" hide-details :label="t('dialogs.add.firstLastPiecePrio')" />
+                <v-checkbox v-model="formData.firstLastPiecePrio"
+                            density="compact"
+                            hide-details
+                            :label="t('dialogs.add.firstLastPiecePrio')" />
               </v-list-item>
             </v-list>
           </v-col>
@@ -278,7 +288,11 @@ watch(
       </v-card-text>
 
       <v-card-actions>
-        <v-btn :disabled="!isFormValid" color="accent" variant="elevated" :text="$t('dialogs.add.submit')" @click="submit" />
+        <v-btn :disabled="!isFormValid"
+               color="accent"
+               variant="elevated"
+               :text="$t('dialogs.add.submit')"
+               @click="submit" />
         <v-btn color="error" variant="flat" :text="$t('common.close')" @click="close" />
       </v-card-actions>
     </v-card>
