f9454890836cd3a8b9cad959460dd3295a2379eb
Larsluph <22910497+Larsluph@users.noreply.github.com>
2023-09-21 06:18:54 +0200

    Add remaining actions on TopContainer

--- src/components/Navbar/TopWidgets/TopContainer.vue
+++ src/components/Navbar/TopWidgets/TopContainer.vue
@@ -1,29 +1,34 @@
 <script setup lang="ts">
 import ConfirmDeleteDialog from '@/components/Dialogs/ConfirmDeleteDialog.vue'
-import { useDashboardStore, useNavbarStore } from '@/stores'
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
+import { useDashboardStore, useMaindataStore, useNavbarStore } from "@/stores";
+import { computed, ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
 import TopActions from './TopActions.vue'
-import TopOverflowMenu from './TopOverflowMenu.vue'
+import TopOverflow from './TopOverflow.vue'
 
+const route = useRoute()
 const router = useRouter()
 const dashboardStore = useDashboardStore()
+const maindataStore = useMaindataStore()
 const navbarStore = useNavbarStore()
 
 const deleteTorrentDialogVisible = ref(false)
 const deleteHashes = ref<string[]>([])
 
-const resumeTorrents = () => {
-  // TODO
-  console.log('resumeTorrents')
+const isOnTorrentDetail = computed(() => route.name === 'torrentDetail')
+const hashes = computed(() => isOnTorrentDetail.value ? [route.params.hash as string] : dashboardStore.selectedTorrents)
+
+const resumeTorrents = async () => {
+  await maindataStore.resumeTorrents(hashes.value)
 }
-const pauseTorrents = () => {
-  // TODO
-  console.log('pauseTorrents')
+const pauseTorrents = async () => {
+  await maindataStore.pauseTorrents(hashes.value)
 }
 const deleteTorrents = () => {
+  if (!hashes.value.length) return
+
   deleteTorrentDialogVisible.value = true
-  deleteHashes.value = [...dashboardStore.selectedTorrents]
+  deleteHashes.value = [...hashes.value]
 }
 const openSearchEngine = () => {
   router.push({ name: 'searchEngine' })
@@ -48,7 +53,7 @@ const openSettings = () => {
 
   <v-divider vertical inset />
 
-  <TopOverflowMenu
+  <TopOverflow
     v-if="$vuetify.display.mobile"
     @resumeTorrents="resumeTorrents"
     @pauseTorrents="pauseTorrents"
