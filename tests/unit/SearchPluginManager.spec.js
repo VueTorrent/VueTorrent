8add48e28c4b92f2fda8a3539a1a10e8d01a6479
Nitin Ramnani <16476523+NitinRamnani@users.noreply.github.com>
2023-10-01 17:35:44 +0530

    Added testcases for Search Plugin Manager

--- tests/unit/SearchPluginManager.spec.js
+++ tests/unit/SearchPluginManager.spec.js
@@ -3,6 +3,7 @@ import Vuetify from 'vuetify'
 import SearchPluginManager from '@/components/Modals/SearchPluginManager.vue'
 import Vuex from 'vuex'
 import Vue from 'vue'
+import { expect, vi } from 'vitest'
 
 Vue.use(Vuetify)
 
@@ -11,28 +12,48 @@ describe('SearchPluginManager.vue', () => {
   localVue.use(Vuex)
   const vuetify = new Vuetify()
   let store
+  let wrapper
 
   beforeEach(() => {
     store = new Vuex.Store({
       actions: {
-        FETCH_SETTINGS: ()=>{},
-        FETCH_SEARCH_PLUGINS: ()=>{}
+        FETCH_SETTINGS: vi.fn(),
+        FETCH_SEARCH_PLUGINS: vi.fn()
       },
       state: {
         searchPlugins: []
       }
     })
-  })
-
-  it('should render the SearchPluginManager', () => {
-    const wrapper = shallowMount(SearchPluginManager, {
+    wrapper  = shallowMount(SearchPluginManager, {
       localVue,
       vuetify,
       store,
       mocks: {
-        $t: () => {}
+        $t: x => x
       }
     })
+  })
+
+  it('should render the SearchPluginManager', () => {
     expect(wrapper.exists()).toBe(true)
   })
+
+  it('should test updatePluginList',async ()=>{
+    const updatePluginListSpy = vi.spyOn(wrapper.vm,'updatePluginList')
+    const dispatchSpy = vi.spyOn(wrapper.vm.$store,'dispatch')
+    await wrapper.vm.updatePluginList()
+    await wrapper.vm.$nextTick()
+    expect(updatePluginListSpy).toHaveBeenCalled()
+    expect(dispatchSpy).toHaveBeenCalledWith('FETCH_SEARCH_PLUGINS')
+  })
+
+  it('should test closeInstallDialog', async ()=>{
+    const closeInstallDialogSpy = vi.spyOn(wrapper.vm,'closeInstallDialog')
+    wrapper.vm.installDialog = true
+    await wrapper.vm.closeInstallDialog()
+    await wrapper.vm.$nextTick()
+    expect(closeInstallDialogSpy).toHaveBeenCalled()
+    expect(wrapper.vm.installDialog).toEqual(false)
+  })
+
 })
