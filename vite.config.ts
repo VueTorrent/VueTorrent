d8ec0c28ebc0041302b34f689afdc7ea112b988c
Daan Wijns <dw.daanwijns@gmail.com>
2023-10-07 22:12:11 +0200

    perf: add pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig, loadEnv } from 'vite'
 import vuetify from 'vite-plugin-vuetify'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -18,7 +19,33 @@ export default defineConfig(({ mode }) => {
     define: {
       'process.env': {}
     },
-    plugins: [vue(), vuetify()],
+    plugins: [
+      vue(), 
+      vuetify(),
+      VitePWA({
+        includeAssets: ['favicon.ico', 'icon.svg', 'icon-192.png', 'icon-512.png', 'robots.txt'],
+        manifest: {
+          name: 'VueTorrent',
+          short_name: 'VueTorrent',
+          theme_color: '#597566',
+          start_url: '.',
+          background_color: '#000',
+          icons: [
+            { src: './icon-192.png', type: 'image/png', sizes: '192x192' },
+            { src: './icon-512.png', type: 'image/png', sizes: '512x512' }
+          ]
+        },
+        // Other options
+        registerType: 'autoUpdate',
+        base: './',
+        useCredentials: true,
+        workbox: {
+          maximumFileSizeToCacheInBytes: 10000000,
+          skipWaiting: true,
+          globPatterns: ['**/*.{js,css,html,ico,png,svg,woff,woff2}']
+        }
+      })
+    ],
     publicDir: './public',
     resolve: {
       alias: {
