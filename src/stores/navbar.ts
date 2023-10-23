e7c29ef12417c8606b203174ef0fda82ad909523
Larsluph <22910497+Larsluph@users.noreply.github.com>
2023-10-23 22:04:35 +0200

    perf(AddTorrentDialog): persist form in sessionStorage

--- src/stores/navbar.ts
+++ src/stores/navbar.ts
@@ -1,14 +1,32 @@
+import { ContentLayout, StopCondition } from '@/constants/qbit/AppPreferences.ts'
+import { usePreferenceStore } from '@/stores/preferences.ts'
+import { Category } from '@/types/qbit/models'
 import { defineStore } from 'pinia'
-import { computed, ref } from 'vue'
+import { computed, reactive, ref } from 'vue'
 
 export const useNavbarStore = defineStore(
   'navbar',
   () => {
+    const preferenceStore = usePreferenceStore()
+
     const downloadData = ref<number[]>(new Array(15).fill(0))
     const uploadData = ref<number[]>(new Array(15).fill(0))
 
     const addTorrentDialogFiles = ref<File[]>([])
     const addTorrentDialogUrls = ref('')
+
+    const addTorrentDialogForm = reactive({
+      autoTMM: false,
+      skipChecking: false,
+      sequentialDownload: false,
+      firstLastPiecePrio: false,
+      startNow: true,
+      contentLayout: ContentLayout.ORIGINAL,
+      stopCondition: StopCondition.NONE,
+      savepath: '',
+      category: null as Category | null,
+      tags: [] as string[]
+    })
 
     const pendingTorrentsCount = computed(() => addTorrentDialogFiles.value.length + addTorrentDialogUrls.value.split('\n').filter(url => url.trim() !== '').length)
 
@@ -33,15 +51,33 @@ export const useNavbarStore = defineStore(
       }
     }
 
+    function resetAddTorrentDialogForm() {
+      addTorrentDialogUrls.value = ''
+      addTorrentDialogFiles.value = []
+
+      addTorrentDialogForm.autoTMM = preferenceStore.preferences!.auto_tmm_enabled
+      addTorrentDialogForm.skipChecking = false
+      addTorrentDialogForm.sequentialDownload = false
+      addTorrentDialogForm.firstLastPiecePrio = false
+      addTorrentDialogForm.startNow = !preferenceStore.preferences!.start_paused_enabled
+      addTorrentDialogForm.contentLayout = preferenceStore.preferences!.torrent_content_layout
+      addTorrentDialogForm.stopCondition = preferenceStore.preferences!.torrent_stop_condition
+      addTorrentDialogForm.savepath = preferenceStore.preferences!.save_path
+      addTorrentDialogForm.category = null
+      addTorrentDialogForm.tags = []
+    }
+
     return {
       downloadData,
       uploadData,
       addTorrentDialogFiles,
       addTorrentDialogUrls,
+      addTorrentDialogForm,
       pendingTorrentsCount,
       pushDownloadData,
       pushUploadData,
-      pushTorrentToQueue
+      pushTorrentToQueue,
+      resetAddTorrentDialogForm
     }
   },
   {
