110c2c4d250531a56a7c146105abf0606ed4cb48
Larsluph <22910497+Larsluph@users.noreply.github.com>
2023-10-24 21:09:16 +0200

    Fix build error

---
+++ src/constants/qbit/FilterState.ts
@@ -0,0 +1,16 @@
+export enum FilterState {
+  ALL = 'all',
+  DOWNLOADING = 'downloading',
+  SEEDING = 'seeding',
+  COMPLETED = 'completed',
+  RESUMED = 'resumed',
+  PAUSED = 'paused',
+  ACTIVE = 'active',
+  INACTIVE = 'inactive',
+  STALLED = 'stalled',
+  STALLED_UPLOADING = 'stalled_uploading',
+  STALLED_DOWNLOADING = 'stalled_downloading',
+  CHECKING = 'checking',
+  MOVING = 'moving',
+  ERRORED = 'errored'
+}
--- src/constants/qbit/index.ts
+++ src/constants/qbit/index.ts
@@ -1,9 +1,10 @@
 import * as AppPreferences from './AppPreferences'
 import { ConnectionStatus } from './ConnectionStatus'
+import { FilterState } from './FilterState'
 import { LogType } from './LogType'
 import { PieceState } from './PieceState'
 import { FilePriority } from './FilePriority'
 import { TorrentState } from './TorrentState'
 import { TrackerStatus } from './TrackerStatus'
 
-export { AppPreferences, ConnectionStatus, LogType, PieceState, FilePriority, TrackerStatus, TorrentState }
+export { AppPreferences, ConnectionStatus, FilterState, LogType, PieceState, FilePriority, TrackerStatus, TorrentState }
