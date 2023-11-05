1f58005109ab5b8a1cf6d9575246e24a105cc3a8
Larsluph <22910497+Larsluph@users.noreply.github.com>
2023-11-05 21:36:52 +0100

    feat(title): Ability to set custom browser tab title

--- src/constants/vuetorrent/TitleOptions.ts
+++ src/constants/vuetorrent/TitleOptions.ts
@@ -1,11 +1,6 @@
 export enum TitleOptions {
   DEFAULT,
   GLOBAL_SPEED,
-  FIRST_TORRENT_STATUS
+  FIRST_TORRENT_STATUS,
+  CUSTOM
 }
-
-export const titleOptionsList = [
-  { title: 'Static Title', value: TitleOptions.DEFAULT },
-  { title: 'Global Speed', value: TitleOptions.GLOBAL_SPEED },
-  { title: 'First Torrent Speed', value: TitleOptions.FIRST_TORRENT_STATUS }
-]
