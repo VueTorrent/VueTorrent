75f6efa33f074027a65abee8a4d777dd8ce0f5d6
WDaan <dw.daanwijns@gmail.com>
2023-11-06 18:02:06 +0000

    Deploying to nightly-release from @ WDaan/VueTorrent@11f6d5944ad13100eaf4b9ea523f985d28efde27 🚀

---
+++ public/assets/MagnetHandler-dbac97ec.js
@@ -0,0 +1,1 @@
+import{as as t}from"./index-4e506bc5.js";import{A as s,a6 as n,_ as r,C as u}from"./vue-aae79161.js";const c=s({__name:"MagnetHandler",setup(m){const a=n(),o=r();return u(async()=>{const e=decodeURIComponent(a.params.url);e.startsWith("magnet:")&&t().pushTorrentToQueue(e),await o.push({name:"dashboard"})}),()=>{}}});export{c as default};
