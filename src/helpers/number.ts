69398e02a5efdeb226486839a5259ea9dbba2fef
Rémi Marseault <22910497+Larsluph@users.noreply.github.com>
2023-12-07 19:00:17 +0100

    fix(helpers): Rework toPrecision (#1394)

--- src/helpers/number.ts
+++ src/helpers/number.ts
@@ -1,12 +1,18 @@
 export function toPrecision(value: number, precision: number): string {
   if (value >= 10 ** precision) {
-    return value.toString()
-  }
-  if (value >= 1) {
-    return value.toPrecision(precision)
+    return Math.floor(value).toString()
   }
 
-  return value.toFixed(precision - 1)
+  const strValue = value.toFixed(precision)
+  if (strValue.length < Math.floor(Math.log10(value)) + 1) {
+    return strValue
+  } else {
+    const result = strValue.substring(0, precision + 1);
+    if (result.endsWith('.')) {
+      return result.slice(0, -1)
+    }
+    return result
+  }
 }
 
 export function formatPercent(progress: number): string {
