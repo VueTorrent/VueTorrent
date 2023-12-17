0e1ec67aad14cee0ccb8642b31d405f1f86efa80
Daan Wijns <dw.daanwijns@gmail.com>
2023-12-17 10:04:32 +0100

    chore: add italian

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -14,6 +14,7 @@ export enum Locales {
   EN = 'en',
   FR = 'fr',
   NL = 'nl',
+  IT = 'it-IT',
   RU = 'ru',
   ZH_HANS = 'zh-Hans',
   ZH_HANT = 'zh-Hant'
@@ -23,6 +24,7 @@ export const LOCALES: LocaleDef[] = [
   { title: 'English', value: Locales.EN },
   { title: 'Français', value: Locales.FR },
   { title: 'Nederlands', value: Locales.NL },
+  { title: 'Italian', value: Locales.IT },
   { title: 'Русский', value: Locales.RU },
   { title: '简体中文', value: Locales.ZH_HANS },
   { title: '繁體中文', value: Locales.ZH_HANT }
@@ -32,6 +34,7 @@ export const messages: Record<Locales, any> = {
   [Locales.EN]: en,
   [Locales.FR]: fr,
   [Locales.NL]: nl,
+  [Locales.IT]: it,
   [Locales.RU]: ru,
   [Locales.ZH_HANS]: zh_hans,
   [Locales.ZH_HANT]: zh_hant
