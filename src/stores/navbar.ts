51d58cfc4962f3c4fcc71d9e65a09863f695e025
Rémi Marseault <22910497+Larsluph@users.noreply.github.com>
2023-12-19 07:57:07 +0100

    perf(SpeedGraph): Init values with null (#1422)

--- src/stores/navbar.ts
+++ src/stores/navbar.ts
@@ -4,8 +4,8 @@ import { ref } from 'vue'
 export const useNavbarStore = defineStore(
   'navbar',
   () => {
-    const downloadData = ref<number[]>(new Array(15).fill(0))
-    const uploadData = ref<number[]>(new Array(15).fill(0))
+    const downloadData = ref<(number | null)[]>(new Array(15).fill(null))
+    const uploadData = ref<(number | null)[]>(new Array(15).fill(null))
 
     function pushDownloadData(data: number) {
       downloadData.value.shift()
