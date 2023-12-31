0dfc06b41d0bfcf54d4593f4eaaf86b3bbe6c5d2
Larsluph <22910497+Larsluph@users.noreply.github.com>
2023-12-31 13:19:08 +0100

    Make MockProvider tree-shakable

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,14 +1,20 @@
 import IProvider from './IProvider'
-import MockProvider from './MockProvider'
 import QBitProvider from './QbitProvider'
 
-function getProvider(): IProvider {
-  if (import.meta.env.DEV && import.meta.env.VITE_USE_MOCK_PROVIDER === 'true')
-    return new MockProvider()
-  else
-    return new QBitProvider()
+async function getProvider(): Promise<IProvider> {
+  if (import.meta.env.DEV && import.meta.env.VITE_USE_MOCK_PROVIDER === 'true') {
+    try {
+      const { default: MockProvider } = await import('./MockProvider')
+      return new MockProvider()
+    } catch (error) {
+      console.error('Failed to load MockProvider:', error)
+      return new QBitProvider()
+    }
+  } else {
+    return new Promise(resolve => resolve(new QBitProvider()))
+  }
 }
 
-const qbit = getProvider()
+const qbit = await getProvider()
 
 export { qbit }
