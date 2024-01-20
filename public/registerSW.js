c5f99047936728960fb88319301da36fb058e160
WDaan <dw.daanwijns@gmail.com>
2024-01-20 14:47:22 +0000

    Deploying to nightly-release from @ VueTorrent/VueTorrent@f4842c444484a2e7eda14bd84ed2fba094a695b0 🚀

--- public/registerSW.js
+++ public/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+"serviceWorker"in navigator&&window.addEventListener("load",(async()=>{await navigator.serviceWorker.register("/sw.js")}));
