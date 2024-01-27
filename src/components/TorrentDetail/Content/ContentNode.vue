8e8f18e9dcb531103ddf1597ac2a8bfe9aa8b717
Larsluph <22910497+Larsluph@users.noreply.github.com>
2024-01-27 19:51:36 +0100

    Generify nodes

--- src/components/TorrentDetail/Content/ContentNode.vue
+++ src/components/TorrentDetail/Content/ContentNode.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
+import { getFileIcon } from '@/constants/vuetorrent'
+import { useI18n } from 'vue-i18n'
 import { FilePriority } from '@/constants/qbit'
-import { doesCommand } from '@/helpers'
-import { useContentStore } from '@/stores'
+import { doesCommand, formatData } from '@/helpers'
+import { useContentStore, useVueTorrentStore } from '@/stores'
 import { TreeNode } from '@/types/vuetorrent'
 import { computed } from 'vue'
-import FileNode from './FileNode.vue'
-import FolderNode from './FolderNode.vue'
 
 const props = defineProps<{
   node: TreeNode
@@ -17,7 +17,11 @@ const emit = defineEmits<{
   onRightClick: [e: MouseEvent | Touch, node: TreeNode]
 }>()
 
+const folderColor = '#ffe476'
+
+const { t } = useI18n()
 const contentStore = useContentStore()
+const vuetorrentStore = useVueTorrentStore()
 
 const depth = computed(() => props.node.fullName.split('/').length)
 
@@ -61,23 +65,75 @@ function getNodeColor(node: TreeNode) {
   const progress = node.getProgress()
   return progress === 1 ? 'green' : ''
 }
+
+function getNodeDeepCount(node: TreeNode) {
+  const [folderCount, fileCount] = node.getDeepCount()
+
+  const res = []
+  if (folderCount > 1) {
+    res.push(t('torrentDetail.content.folderInfo', folderCount - 1))
+  }
+  if (fileCount > 0) {
+    res.push(t('torrentDetail.content.fileInfo', fileCount))
+  }
+
+  return res.join(', ')
+}
+
+function getNodeSubtitle(node: TreeNode) {
+  const values = [formatData(node.getSize(), vuetorrentStore.useBinarySize)]
+
+  if (node.type === 'folder') {
+    values.push(getNodeDeepCount(node))
+  }
+
+  return values.join(' | ')
+}
 </script>
 
 <template>
-  <div :class="['d-flex flex-column py-2 pr-3', (node.isSelected(contentStore.internalSelection)) ? 'bg-grey-darken-3' : '']"
+  <div :class="['d-flex flex-column py-2 pr-3', (node.isSelected(contentStore.internalSelection)) ? 'selected' : '']"
        :style="node.name !== '(root)' && !$vuetify.display.mobile ? `padding-left: ${depth * 32}px` : ''"
        @click.stop="toggleInternalSelection($event, node)"
        @contextmenu="$emit('onRightClick', $event, node)">
-    <FileNode v-if="node.type === 'file'"
-              :node="node"
-              :color="getNodeColor(node)"
-              @toggleFileSelection="toggleFileSelection" />
-    <FolderNode v-else
-                :node="node"
-                :opened-items="openedItems"
-                :color="getNodeColor(node)"
-                @toggleFileSelection="toggleFileSelection"
-                @openNode="openNode" />
+    <div class="d-flex">
+      <!-- Selection checkbox -->
+      <div class="d-flex align-center" @click.stop="toggleFileSelection(node)">
+        <v-icon v-if="node.isWanted() === null" :color="getNodeColor(node)" icon="mdi-checkbox-intermediate-variant" />
+        <v-icon v-else-if="node.isWanted()" :color="getNodeColor(node)" icon="mdi-checkbox-marked" />
+        <v-icon v-else :color="getNodeColor(node)" icon="mdi-checkbox-blank-outline" />
+      </div>
+
+      <!-- Node icons -->
+      <div class="d-flex align-center spacer" @click="openNode($event, node)">
+        <v-icon>{{ openedItems.includes(node.fullName) ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+
+        <v-icon v-if="node.name === '(root)'" icon="mdi-file-tree" />
+        <v-icon v-else-if="node.type === 'file'" :icon="getFileIcon(node.name)" />
+        <v-icon v-else-if="openedItems.includes(node.fullName)" icon="mdi-folder-open" :color="folderColor" />
+        <v-icon v-else icon="mdi-folder" :color="folderColor" />
+      </div>
+
+      <!-- Node content -->
+      <div class="d-flex flex-column overflow-hidden text-no-wrap mr-3">
+        <div :class="`text-${getNodeColor(node)}`">{{ node.name }}</div>
+        <div class="text-grey">
+          {{ getNodeSubtitle(node) }}
+        </div>
+      </div>
+
+      <v-spacer />
+
+      <!-- Priority icon -->
+      <div class="d-flex align-center">
+        <v-icon v-if="node.getPriority() === FilePriority.MAXIMAL" color="error">mdi-arrow-up</v-icon>
+        <v-icon v-else-if="node.getPriority() === FilePriority.HIGH" color="warning">mdi-arrow-top-right</v-icon>
+        <v-icon v-else-if="node.getPriority() === FilePriority.NORMAL">mdi-minus</v-icon>
+        <v-icon v-else-if="node.getPriority() === FilePriority.MIXED">mdi-tilde</v-icon>
+        <v-icon v-else-if="node.getPriority() === FilePriority.DO_NOT_DOWNLOAD" color="grey">mdi-cancel</v-icon>
+      </div>
+    </div>
+    <v-progress-linear :model-value="node.getProgress()" :max="1" :color="getNodeColor(node)" rounded="sm" />
   </div>
 </template>
 
@@ -85,5 +141,15 @@ function getNodeColor(node: TreeNode) {
 .spacer {
   margin-left: 8px;
   margin-right: 16px;
+}
+
+.v-theme--darkTheme .selected {
+  background-color: rgb(var(--v-theme-surface));
+  filter: brightness(125%);
+}
+
+.v-theme--lightTheme .selected {
+  background-color: rgb(var(--v-theme-surface));
+  filter: brightness(75%);
 }
 </style>
