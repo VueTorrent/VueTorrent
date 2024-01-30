511e5b769a06109c3149d935ccfe93f78e363ae6
Rémi Marseault <22910497+Larsluph@users.noreply.github.com>
2024-01-30 08:01:58 +0100

    fix(progress): Remove striping on table view

--- src/components/Dashboard/Views/Table/DashboardItems/ItemPercent.vue
+++ src/components/Dashboard/Views/Table/DashboardItems/ItemPercent.vue
@@ -4,27 +4,12 @@ import { formatPercent } from '@/helpers'
 import { Torrent } from '@/types/vuetorrent'
 import { computed } from 'vue'
 
-const props = defineProps<{ torrent: Torrent; title: string; value: string }>()
-
-const isTorrentActive = computed(() => {
-  return [
-    TorrentState.UPLOADING,
-    TorrentState.CHECKING_UP,
-    TorrentState.FORCED_UP,
-    TorrentState.ALLOCATING,
-    TorrentState.DOWNLOADING,
-    TorrentState.META_DL,
-    TorrentState.CHECKING_DL,
-    TorrentState.FORCED_DL,
-    TorrentState.CHECKING_RESUME_DATA,
-    TorrentState.MOVING
-  ].includes(props.torrent.state)
-})
+defineProps<{ torrent: Torrent; title: string; value: string }>()
 </script>
 
 <template>
   <td>
-    <v-progress-linear :model-value="torrent[value]" :max="1" :striped="isTorrentActive" :height="20" :color="`torrent-${torrent.state}`" rounded="sm" style="width: 10em">
+    <v-progress-linear :model-value="torrent[value]" :max="1" :height="20" :color="`torrent-${torrent.state}`" rounded="sm" style="width: 10em">
       {{ formatPercent(torrent[value]) }}
     </v-progress-linear>
   </td>
