d750370282d3c6d0df7daf96e3495511ba0d9ae4
Rémi Marseault <22910497+Larsluph@users.noreply.github.com>
2024-02-06 09:36:52 +0100

    perf(SpeedGraph): Add time as X axis instead of computed step (#1494)

--- src/plugins/dayjs.ts
+++ src/plugins/dayjs.ts
@@ -1,10 +1,12 @@
 import dayjs from 'dayjs'
 import duration from 'dayjs/plugin/duration'
 import relativeTime from 'dayjs/plugin/relativeTime'
+import updateLocale from 'dayjs/plugin/updateLocale'
 
 const config = {
   thresholds: [
     { l: 's', r: 1 },
+    { l: 'ss', r: 59, d: 'second' },
     { l: 'm', r: 1 },
     { l: 'mm', r: 59, d: 'minute' },
     { l: 'h', r: 1 },
@@ -21,5 +23,25 @@ const config = {
 
 dayjs.extend(duration)
 dayjs.extend(relativeTime, config)
+dayjs.extend(updateLocale)
+
+dayjs.updateLocale('en', {
+  relativeTime: {
+    future: 'in %s',
+    past: '%s ago',
+    s: 'a few seconds',
+    ss: '%d seconds',
+    m: 'a minute',
+    mm: '%d minutes',
+    h: 'an hour',
+    hh: '%d hours',
+    d: 'a day',
+    dd: '%d days',
+    M: 'a month',
+    MM: '%d months',
+    y: 'a year',
+    yy: '%d years'
+  }
+})
 
 export default dayjs
