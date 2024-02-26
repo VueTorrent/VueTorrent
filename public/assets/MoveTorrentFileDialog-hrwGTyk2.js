539906063e86349d5c9a924fce3e748493ca8f9b
WDaan <dw.daanwijns@gmail.com>
2024-02-26 10:21:05 +0000

    Deploying to latest-release from @ VueTorrent/VueTorrent@82d4bd86e3b7518cba30051a21f35a46639ec6ca 🚀

---
+++ public/assets/MoveTorrentFileDialog-hrwGTyk2.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./MoveTorrentFileDialog.vue_vue_type_script_setup_true_lang-YGVMdjAZ.js";import"./vue-YxIjAFFl.js";import"./index-4rOwniJ2.js";import"./vuetify-yc6oP1mJ.js";export{o as default};
--- public/registerSW.js
+++ public/registerSW.js
@@ -1,4 +1,1 @@
-'serviceWorker' in navigator &&
-  window.addEventListener('load', async () => {
-    await navigator.serviceWorker.register('/sw.js')
-  })
+"serviceWorker"in navigator&&window.addEventListener("load",(async()=>{await navigator.serviceWorker.register("/sw.js")}))
