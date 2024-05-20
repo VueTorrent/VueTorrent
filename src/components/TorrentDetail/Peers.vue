6fa09e1a71808702f406525ef19b5f438100da37
Larsluph <22910497+Larsluph@users.noreply.github.com>
2024-05-20 17:11:09 +0200

    update filters

--- src/components/TorrentDetail/Peers.vue
+++ src/components/TorrentDetail/Peers.vue
@@ -128,9 +128,9 @@ onBeforeRouteLeave(() => !addPeersDialog.value)
 <template>
   <v-card>
     <v-empty-state v-if="!torrentPeers.size" :title="$t('torrentDetail.peers.empty')" icon="mdi-account-sync" color="accent" />
-    <v-data-table v-else :headers="headers" :items="items" multi-sort :sort-by="sortBy" :search="filter" :filter-keys="['host']" :mobile="null">
+    <v-data-table v-else :headers="headers" :items="items" multi-sort :sort-by="sortBy" :search="filter" :filter-keys="['host', 'client', 'files']" :mobile="null">
       <template #top>
-        <v-text-field v-model="filter" class="ma-3" density="compact" :label="$t('common.search')" prepend-inner-icon="mdi-magnify" flat hide-details single-line />
+        <v-text-field v-model="filter" class="ma-3" density="compact" :label="$t('common.search')" prepend-inner-icon="mdi-magnify" flat hide-details single-line clearable />
       </template>
 
       <template #[`item.host`]="{ item }">
