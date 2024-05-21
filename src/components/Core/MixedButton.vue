7893ecdd428ec8572b8f3569c5a8a4a79997a547
Larsluph <22910497+Larsluph@users.noreply.github.com>
2024-05-22 00:31:29 +0200

    Update MixedButton tests

--- src/components/Core/MixedButton.vue
+++ src/components/Core/MixedButton.vue
@@ -25,8 +25,8 @@ const mobile = computed(() => (props.mobileOverride ? props.mobileValue : mobile
 
 <template>
   <v-btn :icon="mobile">
-    <v-icon v-if="mobile || position === 'left'" data-testid="mixedbtn-icon">{{ icon }}</v-icon>
+    <v-icon v-if="mobile || position === 'left'" data-testid="mixedbtn-icon-left">{{ icon }}</v-icon>
     <span v-if="!mobile" data-testid="mixedbtn-text">{{ text }}</span>
-    <v-icon v-if="!mobile && position === 'right'" data-testid="mixedbtn-icon">{{ icon }}</v-icon>
+    <v-icon v-if="!mobile && position === 'right'" data-testid="mixedbtn-icon-right">{{ icon }}</v-icon>
   </v-btn>
 </template>
