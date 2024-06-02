73a9c3051040ed6b67db1f4d8ca29d500d9cf6f8
Larsluph <22910497+Larsluph@users.noreply.github.com>
2024-06-02 09:44:15 +0200

    fix(chips): Use text instead of invisible pill

--- src/components/Core/ColoredChip.vue
+++ src/components/Core/ColoredChip.vue
@@ -4,6 +4,7 @@ import { useVueTorrentStore } from '@/stores'
 import { computed } from 'vue'
 import { getColorFromName } from '@/helpers'
 import { useI18n } from 'vue-i18n'
+import { useTheme } from 'vuetify'
 
 const props = withDefaults(
   defineProps<{
@@ -18,15 +19,24 @@ const props = withDefaults(
 )
 
 const { t } = useI18n()
+const { current } = useTheme()
 const { enableHashColors, hideColoredChip } = storeToRefs(useVueTorrentStore())
 
+function getThemeColor(color: string) {
+  return current.value.colors[color] ?? color
+}
+
 const chipColor = computed(() => (props.disabled || !enableHashColors.value ? props.defaultColor : getColorFromName(props.value)))
-const chipValue = computed(() => (props.disabled ? props.disabledValue || t('common.none') : props.value))
+const rawChipColor = computed(() => (props.disabled || !enableHashColors.value ? getThemeColor(props.defaultColor) : getColorFromName(props.value)))
+const chipValue = computed(() => (props.disabled ? props.disabledValue || props.value || t('common.none') : props.value))
 const shouldShowColoredChips = computed(() => !hideColoredChip.value)
 </script>
 
 <template>
-  <v-chip :color="chipColor" :variant="shouldShowColoredChips ? 'flat' : 'text'">
+  <v-chip v-if="shouldShowColoredChips" :color="chipColor" variant="flat">
     {{ chipValue }}
   </v-chip>
+  <div v-else class="text-no-wrap" :style="`color: ${rawChipColor}`">
+    {{ chipValue }}
+  </div>
 </template>
--- src/components/Dashboard/Views/Table/DashboardItems/ItemChip.vue
+++ src/components/Dashboard/Views/Table/DashboardItems/ItemChip.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { getColorFromName } from '@/helpers'
+import ColoredChip from '@/components/Core/ColoredChip.vue'
 import { useVueTorrentStore } from '@/stores'
 import { Torrent } from '@/types/vuetorrent'
 import { storeToRefs } from 'pinia'
@@ -18,24 +18,27 @@ const props = withDefaults(
   }
 )
 
-const { hideChipIfUnset, enableHashColors, hideColoredChip } = storeToRefs(useVueTorrentStore())
+const { hideChipIfUnset } = storeToRefs(useVueTorrentStore())
 
 const val = computed(() => props.value(props.torrent))
 const emptyValue = computed(() => val.value.length < 1 || val.value[0] === '')
 const shouldShowChip = computed(() => !(hideChipIfUnset.value && emptyValue.value))
-const shouldShowColoredChips = computed(() => !hideColoredChip.value)
-const useRandomColor = computed(() => enableHashColors.value && props.enableHashColor)
 </script>
 
 <template>
   <td>
     <div class="d-flex flex-row flex-gap-column-small" v-if="shouldShowChip">
-      <v-chip v-if="emptyValue" :color="color(torrent)" :variant="shouldShowColoredChips ? 'flat' : 'text'" size="small">
-        {{ $t(emptyValueKey) }}
-      </v-chip>
-      <v-chip v-else v-for="v in val" :color="useRandomColor ? getColorFromName(v) : color(torrent)" :variant="shouldShowColoredChips ? 'flat' : 'text'" size="small">
-        {{ v }}
-      </v-chip>
+      <ColoredChip v-if="emptyValue"
+                   :disabled="true"
+                   :default-color="color(torrent)"
+                   :value="$t(emptyValueKey)"
+                   size="small" />
+      <ColoredChip v-else
+                   v-for="v in val"
+                   :disabled="!enableHashColor"
+                   :default-color="color(torrent)"
+                   :value="v"
+                   size="small" />
     </div>
   </td>
 </template>
