35cb68a506e18a8c08093c0197ab23a2830f9ec5
Larsluph <22910497+Larsluph@users.noreply.github.com>
2024-06-30 16:04:35 +0200

    perf(content): Group numbers when sorting filenames

--- src/helpers/comparators.ts
+++ src/helpers/comparators.ts
@@ -1,55 +1,65 @@
-export default {
-  numeric: {
-    asc: (a: number, b: number) => a - b,
-    desc: (a: number, b: number) => b - a
-  },
-  invertedNumeric: {
-    asc: (a: number, b: number) => b - a,
-    desc: (a: number, b: number) => a - b
-  },
-  text: {
-    asc: (a: string, b: string) => a.localeCompare(b),
-    desc: (a: string, b: string) => b.localeCompare(a)
-  },
-  boolean: {
-    asc: (a: boolean, b: boolean) => (a === b ? 0 : a ? 1 : -1),
-    desc: (a: boolean, b: boolean) => (a === b ? 0 : a ? -1 : 1)
-  },
-  arrayNumeric: {
-    asc: (a: number[], b: number[]) => {
-      for (let i = 0; i < Math.min(a.length, b.length); i++) {
-        const diff = a[i] - b[i]
-        if (diff) return diff
-      }
-      return a.length - b.length
-    },
-    desc: (a: number[], b: number[]) => {
-      for (let i = 0; i < Math.min(a.length, b.length); i++) {
-        const diff = b[i] - a[i]
-        if (diff) return diff
-      }
-      return b.length - a.length
-    }
-  },
-  arrayText: {
-    asc: (a: string[], b: string[]) => {
-      for (let i = 0; i < Math.min(a.length, b.length); i++) {
-        const diff = a[i].localeCompare(b[i])
-        if (diff) return diff
-      }
-      return a.length - b.length
-    },
-    desc: (a: string[], b: string[]) => {
-      for (let i = 0; i < Math.min(a.length, b.length); i++) {
-        const diff = b[i].localeCompare(a[i])
-        if (diff) return diff
-      }
-      return b.length - a.length
-    }
+export class Comparator<T = any> {
+  asc: (a: T, b: T) => number
+
+  constructor(asc: (a: T, b: T) => number) {
+    this.asc = asc
+  }
+
+  desc(a: T, b: T) {
+    return -this.asc(a, b)
   }
 }
 
-export type Comparator<T = any> = {
-  asc: (a: T, b: T) => number
-  desc: (a: T, b: T) => number
+type ComparatorMap = {
+  /** Compare numeric values */
+  numeric: Comparator<number>
+  /** Compare numeric values in reverse order */
+  invertedNumeric: Comparator<number>
+  /** Compare strings */
+  text: Comparator<string>
+  /** Compare strings while grouping numeric values */
+  textWithNumbers: Comparator<string>
+  /** Compare booleans */
+  boolean: Comparator<boolean>
+  /** Compare arrays of numeric values */
+  arrayNumeric: Comparator<number[]>
+  /** Compare arrays of strings */
+  arrayText: Comparator<string[]>
 }
+
+const comparators: ComparatorMap = {
+  numeric: new Comparator((a: number, b: number) => a - b),
+  invertedNumeric: new Comparator((a: number, b: number) => b - a),
+  text: new Comparator((a: string, b: string) => a.localeCompare(b)),
+  textWithNumbers: new Comparator((a: string, b: string) => {
+    const pattern = /(\d+)/
+    const as = a.split(pattern)
+    const bs = b.split(pattern)
+    for (let i = 0; i < Math.min(as.length, bs.length); i++) {
+      if (i % 2 === 1) {
+        const diff = +as[i] - +bs[i]
+        if (diff !== 0) return diff
+      } else {
+        if (as[i] !== bs[i]) return as[i].localeCompare(bs[i])
+      }
+    }
+    return as.length - bs.length
+  }),
+  boolean: new Comparator((a: boolean, b: boolean) => (a === b ? 0 : a ? 1 : -1)),
+  arrayNumeric: new Comparator((a: number[], b: number[]) => {
+    for (let i = 0; i < Math.min(a.length, b.length); i++) {
+      const diff = a[i] - b[i]
+      if (diff) return diff
+    }
+    return a.length - b.length
+  }),
+  arrayText: new Comparator((a: string[], b: string[]) => {
+    for (let i = 0; i < Math.min(a.length, b.length); i++) {
+      const diff = a[i].localeCompare(b[i])
+      if (diff) return diff
+    }
+    return a.length - b.length
+  })
+}
+
+export default comparators
