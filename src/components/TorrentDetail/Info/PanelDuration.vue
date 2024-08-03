138b5620cc246619f4a9265cc7a7a8692ff3c9a2
Rémi Marseault <22910497+Larsluph@users.noreply.github.com>
2024-08-02 23:37:31 -0500

    Update PanelDuration.vue

--- src/components/TorrentDetail/Info/PanelDuration.vue
+++ src/components/TorrentDetail/Info/PanelDuration.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import dayjs from '@/plugins/dayjs'
-import { useTorrentDetailStore } from '@/stores'
+import { formatDuration } from '@/helpers'
+import { useTorrentDetailStore, useVueTorrentStore } from '@/stores'
 import { Torrent } from '@/types/vuetorrent'
 import { DurationUnitType } from 'dayjs/plugin/duration'
 import { storeToRefs } from 'pinia'
@@ -9,6 +9,7 @@ import InfoBase from './InfoBase.vue'
 const props = defineProps<{ torrent: Torrent }>()
 
 const { properties } = storeToRefs(useTorrentDetailStore())
+const { durationFormat } = storeToRefs(useVueTorrentStore())
 
 const torrentValues: { title: string; unit: DurationUnitType; getter: () => number }[] = [
   { title: 'seeding_time', unit: 's', getter: () => props.torrent.seeding_time },
@@ -26,7 +27,7 @@ const torrentValues: { title: string; unit: DurationUnitType; getter: () => numb
         <InfoBase v-for="ppt in torrentValues">
           <template v-slot:title>{{ $t(`torrent.properties.${ ppt.title }`) }}</template>
           <template v-if="ppt.getter() > 0" v-slot:text>
-            {{ dayjs.duration(ppt.getter(), ppt.unit).humanize() }}
+            {{ formatDuration(ppt.getter(), ppt.unit, durationFormat) }}
           </template>
           <template v-else v-slot:text>{{ $t('common.NA') }}</template>
         </InfoBase>
