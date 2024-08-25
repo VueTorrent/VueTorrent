0d1d273d163dd33794be6753ed69de7e1fbf153e
Rémi Marseault <22910497+Larsluph@users.noreply.github.com>
2024-08-25 00:49:59 -0500

    chore: Suppress discord webhook for pre-releases

--- write-version.cjs
+++ write-version.cjs
@@ -2,7 +2,7 @@ const { execSync } = require('child_process')
 const fs = require('fs')
 const path = require('path')
 
-const version = execSync('git describe --tags --exclude nightly_release').toString().trim()
+const version = execSync('git describe --tags --exclude latest_nightly').toString().trim()
 
 const filePath = path.join(__dirname, 'vuetorrent', 'version.txt')
 fs.writeFileSync(filePath, version)
