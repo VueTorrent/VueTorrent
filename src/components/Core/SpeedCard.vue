5a0ad417dc8b6d4dc64c15eb2ba50f8b5e4ffafc
Jagadam Dinesh Reddy <dineshjagadam@gmail.com>
2024-11-14 19:49:45 +0530

    feat(Navbar): Make speed cards clickable to select dl & ul filters (#2043)
    Co-authored-by: Rémi Marseault <larsluph@larsluph.dev>

--- src/components/Core/SpeedCard.vue
+++ src/components/Core/SpeedCard.vue
@@ -2,17 +2,24 @@
 import { formatSpeedUnit, formatSpeedValue } from '@/helpers'
 import { useVueTorrentStore } from '@/stores'
 
-defineProps<{
+withDefaults(defineProps<{
   icon: string
   color: string
   value: number
+  active?: boolean
+}>(), {
+  active: false
+})
+
+defineEmits<{
+  click: [MouseEvent]
 }>()
 
 const vueTorrentStore = useVueTorrentStore()
 </script>
 
 <template>
-  <v-sheet color="secondary" rounded="lg" class="py-3">
+  <v-sheet :class="{ 'cursor-pointer': !!$.vnode.props?.onClick }" :color="active ? 'secondary-lighten-1' : 'secondary'" rounded="lg" class="py-3" @click.stop="$emit('click', $event)">
     <v-row class="d-flex flex-row align-center px-3">
       <v-col cols="2" class="px-4">
         <v-icon class="" :icon="icon" :color="color" />
--- src/components/Navbar/SideWidgets/CurrentSpeed.vue
+++ src/components/Navbar/SideWidgets/CurrentSpeed.vue
@@ -1,12 +1,33 @@
 <script setup lang="ts">
 import SpeedCard from '@/components/Core/SpeedCard.vue'
-import { useMaindataStore, useVueTorrentStore } from '@/stores'
-import { storeToRefs } from 'pinia'
 import { useI18nUtils } from '@/composables'
+import { TorrentState } from '@/constants/vuetorrent'
+import { useMaindataStore, useTorrentStore, useVueTorrentStore } from '@/stores'
+import { storeToRefs } from 'pinia'
+import { computed } from 'vue'
 
 const { t } = useI18nUtils()
 const { serverState } = storeToRefs(useMaindataStore())
 const { displayGraphLimits } = storeToRefs(useVueTorrentStore())
+const { statusFilter } = storeToRefs(useTorrentStore())
+
+const downloadingStates = [TorrentState.DOWNLOADING, TorrentState.DL_FORCED]
+const uploadingStates = [TorrentState.UPLOADING, TorrentState.UL_FORCED]
+
+function removeFilter() {
+  statusFilter.value = []
+}
+
+function selectDownloading() {
+  statusFilter.value = downloadingStates
+}
+
+function selectUploading() {
+  statusFilter.value = uploadingStates
+}
+
+const isDownloadingFilterActive = computed(() => downloadingStates.every(state => statusFilter.value.includes(state)) && downloadingStates.length === statusFilter.value.length)
+const isUploadingFilterActive = computed(() => uploadingStates.every(state => statusFilter.value.includes(state)) && uploadingStates.length === statusFilter.value.length)
 </script>
 
 <template>
@@ -18,10 +39,20 @@ const { displayGraphLimits } = storeToRefs(useVueTorrentStore())
       <v-sheet class="mx-2 inherit-bg">
         <v-row class="pt-0">
           <v-col cols="6" class="px-1 pt-1">
-            <SpeedCard icon="mdi-arrow-down" color="download" :value="serverState?.dl_info_speed ?? 0" />
+            <SpeedCard
+              icon="mdi-arrow-down"
+              color="download"
+              :value="serverState?.dl_info_speed ?? 0"
+              :active="isDownloadingFilterActive"
+              @click="isDownloadingFilterActive ? removeFilter() : selectDownloading()" />
           </v-col>
           <v-col cols="6" class="px-1 pt-1">
-            <SpeedCard icon="mdi-arrow-up" color="upload" :value="serverState?.up_info_speed ?? 0" />
+            <SpeedCard
+              icon="mdi-arrow-up"
+              color="upload"
+              :value="serverState?.up_info_speed ?? 0"
+              :active="isUploadingFilterActive"
+              @click="isUploadingFilterActive ? removeFilter() : selectUploading()" />
           </v-col>
 
           <template v-if="displayGraphLimits && (serverState?.dl_rate_limit || serverState?.up_rate_limit)">
@@ -40,5 +71,3 @@ const { displayGraphLimits } = storeToRefs(useVueTorrentStore())
     </v-card-text>
   </v-card>
 </template>
-
-<style scoped></style>
