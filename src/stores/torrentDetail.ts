1c9af822a3bd61ae5fe5b79211ab1a65c9d1def3
Rémi Marseault <larsluph@larsluph.dev>
2024-12-08 20:42:09 +0100

    perf(TorrentDetail): Persist last opened tab

--- src/stores/torrentDetail.ts
+++ src/stores/torrentDetail.ts
@@ -3,21 +3,32 @@ import { TorrentProperties } from '@/types/qbit/models'
 import { acceptHMRUpdate, defineStore } from 'pinia'
 import { ref } from 'vue'
 
-export const useTorrentDetailStore = defineStore('torrentDetail', () => {
-  const properties = ref<TorrentProperties>()
+export const useTorrentDetailStore = defineStore(
+  'torrentDetail',
+  () => {
+    const tab = ref('overview')
+    const properties = ref<TorrentProperties>()
 
-  async function fetchProperties(hash: string) {
-    properties.value = await qbit.getTorrentProperties(hash)
-  }
+    async function fetchProperties(hash: string) {
+      properties.value = await qbit.getTorrentProperties(hash)
+    }
 
-  return {
-    properties,
-    fetchProperties,
-    $reset: () => {
-      properties.value = undefined
+    return {
+      tab,
+      properties,
+      fetchProperties,
+      $reset: () => {
+        properties.value = undefined
+      }
+    }
+  },
+  {
+    persistence: {
+      enabled: true,
+      storageItems: [{ storage: localStorage, includePaths: ['tab'] }]
     }
   }
-})
+)
 
 if (import.meta.hot) {
   import.meta.hot.accept(acceptHMRUpdate(useTorrentDetailStore, import.meta.hot))
