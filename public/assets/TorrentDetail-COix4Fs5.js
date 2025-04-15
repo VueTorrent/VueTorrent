7d6f4f7dfb0de10722a0f07c388608e5426d1126
WDaan <daan.wijns@accelins.com>
2025-04-15 07:37:28 +0000

    Deploying to nightly-release from @ VueTorrent/VueTorrent@eb4b0bfdee52700f3a482dc1d51921676a89232e 🚀

--- public/assets/TorrentDetail-COix4Fs5.js
+++ public/assets/TorrentDetail-COix4Fs5.js
@@ -1,7 +1,7 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./MoveTorrentFileDialog-B0Ub9PCd.js","./ColoredChip.vue_vue_type_script_setup_true_lang-DvcegW-8.js","./index-NNID2ue4.js","./vue-CNrckKKA.js","./vuetify-CQ2RXDiw.js","./index-BWcPOIuC.css","./BulkRenameFilesDialog-CNMENgLT.js","./BulkRenameFilesDialog-CKEu44go.css","./browserAll-CYcPiTZs.js","./webworkerAll-BinDn4az.js","./colorToUniform-C2GHuDhf.js","./CanvasPool-BenTHHWr.js","./WebGPURenderer-CPoFkp46.js","./SharedSystems-BQ9N7Qsq.js","./WebGLRenderer-DjrLfbZ_.js"])))=>i.map(i=>d[i]);
-import { b as Ce, a as Io, _ as Ro } from "./ColoredChip.vue_vue_type_script_setup_true_lang-DvcegW-8.js";
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./MoveTorrentFileDialog-t1tgd_at.js","./ColoredChip.vue_vue_type_script_setup_true_lang-BF0FjzIi.js","./index-BiG3EGgd.js","./vue-CNrckKKA.js","./vuetify-CQ2RXDiw.js","./index-BWcPOIuC.css","./BulkRenameFilesDialog-CF9ABF5C.js","./BulkRenameFilesDialog-CKEu44go.css","./browserAll-CQs1Iu3H.js","./webworkerAll-29z8k3B_.js","./colorToUniform-C2GHuDhf.js","./CanvasPool-XWi2Yi7s.js","./WebGPURenderer-C_PTV4EE.js","./SharedSystems-PpeSPzsC.js","./WebGLRenderer-CH-Wk_If.js"])))=>i.map(i=>d[i]);
+import { b as Ce, a as Io, _ as Ro } from "./ColoredChip.vue_vue_type_script_setup_true_lang-BF0FjzIi.js";
 import { s as Yt, a as Z, a7 as Or, b as Eo, a1 as tn, d as mt, W as Do, R as xt, r as vs, l as Bo, B as We, a8 as nr, h as wt, I as Fo, D as j, E as $, H as w, m as y, J as X, K as N, u as M, M as Ye, N as Et, L as en, P as Xe, a9 as Go, n as pe, T as W, O as K, V as sn, Y as gs, w as ke, A as ts, q as ws, a0 as zr, F as kt, U as Gt, G as rn, a2 as Tr, S as No, X as Lr, v as Vo, aa as nn, ab as on, Q as $o } from "./vue-CNrckKKA.js";
-import { br as nt, bs as Oo, a2 as an, a0 as Se, u as ee, F as Cs, o as jt, a3 as zo, af as ln, p as Ss, bt as Pe, c as hn, d as Xt, e as As, f as me, g as un, V as Qt, i as je, j as Me, k as ot, l as qe, bu as Ur, bv as Lo, bw as Hr, bx as Uo, J as Ho, K as Wo, r as pt, q as it, t as Wr, ax as or, a as St, w as Yo, s as Ie, by as Xo, a7 as Ot, S as jo, ai as cn, bq as qo, a9 as Ts, R as _s, W as Ko, ay as Zo, X as ks, z as ge, B as _e, ad as be, a6 as Qo, bg as Jo, C as ys, ao as Yr, bm as fe, y as ta, bz as Es, bj as ea, bf as sa, be as ra, bn as Xr, bA as ia, bB as na, bC as oa, H as aa, aq as la, aj as ar, ba as ha, aQ as ua, av as ca, ah as lr, a1 as dn, bD as da, bE as Ds, ae as fn, b as fa, m as pa, b4 as ma, bF as ga, a4 as _a, a5 as ya, b2 as ve, b3 as xa, __tla as __tla_0 } from "./index-NNID2ue4.js";
+import { br as nt, bs as Oo, a2 as an, a0 as Se, u as ee, F as Cs, o as jt, a3 as zo, af as ln, p as Ss, bt as Pe, c as hn, d as Xt, e as As, f as me, g as un, V as Qt, i as je, j as Me, k as ot, l as qe, bu as Ur, bv as Lo, bw as Hr, bx as Uo, J as Ho, K as Wo, r as pt, q as it, t as Wr, ax as or, a as St, w as Yo, s as Ie, by as Xo, a7 as Ot, S as jo, ai as cn, bq as qo, a9 as Ts, R as _s, W as Ko, ay as Zo, X as ks, z as ge, B as _e, ad as be, a6 as Qo, bg as Jo, C as ys, ao as Yr, bm as fe, y as ta, bz as Es, bj as ea, bf as sa, be as ra, bn as Xr, bA as ia, bB as na, bC as oa, H as aa, aq as la, aj as ar, ba as ha, aQ as ua, av as ca, ah as lr, a1 as dn, bD as da, bE as Ds, ae as fn, b as fa, m as pa, b4 as ma, bF as ga, a4 as _a, a5 as ya, b2 as ve, b3 as xa, __tla as __tla_0 } from "./index-BiG3EGgd.js";
 import { a as pn, u as ba } from "./vuetify-CQ2RXDiw.js";
 let Ft, no, Pt, xe, ne, ht, Tn, Ms, zt, $l, ui, Ke, ch, tt, bh, _t, At, vt, yr, Wh, ns, mr, Sh, di, Hs, pi, Ll, Ca, re, Mn, oo, ct, yt, Mh, Zh, gu, yu, Cu, Au, Tu, Ne, ze, Qi, Is, Rh, xs, Te, vo, Ii, Pi, al, bs, hl, So, _d, Qe, ps, Rr, Nt, Mi, Lh, so, On, qt, eo, mu, _u, Qr, vu, Su, uo, wl, Er, ut, ye, Zn, Dt, ll, Pu, Jt;
 let __tla = Promise.all([
@@ -245,7 +245,7 @@ let __tla = Promise.all([
         }
         async function O(k) {
             const { default: E } = await Pe(async ()=>{
-                const { default: H } = await import("./MoveTorrentFileDialog-B0Ub9PCd.js");
+                const { default: H } = await import("./MoveTorrentFileDialog-t1tgd_at.js");
                 return {
                     default: H
                 };
@@ -258,7 +258,7 @@ let __tla = Promise.all([
         }
         async function R(k) {
             const { default: E } = await Pe(async ()=>{
-                const { default: H } = await import("./BulkRenameFilesDialog-CNMENgLT.js");
+                const { default: H } = await import("./BulkRenameFilesDialog-CF9ABF5C.js");
                 return {
                     default: H
                 };
@@ -2213,7 +2213,7 @@ let __tla = Promise.all([
         },
         test: ()=>!0,
         load: async ()=>{
-            await Pe(()=>import("./browserAll-CYcPiTZs.js"), __vite__mapDeps([8,9,10,11,1,2,3,4,5]), import.meta.url);
+            await Pe(()=>import("./browserAll-CQs1Iu3H.js"), __vite__mapDeps([8,9,10,11,1,2,3,4,5]), import.meta.url);
         }
     };
     Ka = {
@@ -2224,7 +2224,7 @@ let __tla = Promise.all([
         },
         test: ()=>typeof self < "u" && self.WorkerGlobalScope !== void 0,
         load: async ()=>{
-            await Pe(()=>import("./webworkerAll-BinDn4az.js"), __vite__mapDeps([9,10,11,1,2,3,4,5]), import.meta.url);
+            await Pe(()=>import("./webworkerAll-29z8k3B_.js"), __vite__mapDeps([9,10,11,1,2,3,4,5]), import.meta.url);
         }
     };
     class Rt {
@@ -6965,7 +6965,7 @@ ${r}`;
             const o = t[n];
             if (o === "webgpu" && await kh()) {
                 const { WebGPURenderer: a } = await Pe(async ()=>{
-                    const { WebGPURenderer: l } = await import("./WebGPURenderer-CPoFkp46.js");
+                    const { WebGPURenderer: l } = await import("./WebGPURenderer-C_PTV4EE.js");
                     return {
                         WebGPURenderer: l
                     };
@@ -6977,7 +6977,7 @@ ${r}`;
                 break;
             } else if (o === "webgl" && Th(r.failIfMajorPerformanceCaveat ?? no.defaultOptions.failIfMajorPerformanceCaveat)) {
                 const { WebGLRenderer: a } = await Pe(async ()=>{
-                    const { WebGLRenderer: l } = await import("./WebGLRenderer-DjrLfbZ_.js");
+                    const { WebGLRenderer: l } = await import("./WebGLRenderer-CH-Wk_If.js");
                     return {
                         WebGLRenderer: l
                     };
