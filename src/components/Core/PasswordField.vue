43b55ac58a5a6fef95181dd28c62164f04634718
Z ❤'s Life <96372751+zcraftelite9495@users.noreply.github.com>
2025-06-29 11:44:07 -0400

    Fix Password Manager Autofill (#2358)

--- src/components/Core/PasswordField.vue
+++ src/components/Core/PasswordField.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { computed, ref, useAttrs } from 'vue'
 
 defineProps<{
-  hideIcon?: boolean
-  prependIcon?: string
+    hideIcon?: boolean
+    prependIcon?: string
 }>()
 
+const attrs = useAttrs()
 const showPassword = ref(false)
 
 function toggleShow() {
-  showPassword.value = !showPassword.value
+    showPassword.value = !showPassword.value
 }
 
 const type = computed(() => (showPassword.value ? 'text' : 'password'))
@@ -17,11 +18,17 @@ const icon = computed(() => (showPassword.value ? 'mdi-eye' : 'mdi-eye-off'))
 </script>
 
 <template>
-  <v-text-field name="password" :type="type" :append-inner-icon="hideIcon ? '' : icon" @click:append-inner="toggleShow">
-    <template v-if="prependIcon" #prepend>
-      <v-icon color="accent" :icon="prependIcon" />
-    </template>
-  </v-text-field>
+    <v-text-field
+        v-bind="attrs"
+        name="password"
+        :type="type"
+        :append-inner-icon="hideIcon ? '' : icon"
+        @click:append-inner="toggleShow"
+    >
+        <template v-if="prependIcon" #prepend>
+            <v-icon color="accent" :icon="prependIcon" />
+        </template>
+    </v-text-field>
 </template>
 
 <style scoped></style>
