4cd3ab07102c6555cbfbc398138e8da7bc2a5259
Rémi Marseault <larsluph@larsluph.dev>
2025-07-20 10:14:41 +0200

    fix lint

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,7 +1,6 @@
 import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import themeMap, { DarkRedesigned } from '@/themes'
 
