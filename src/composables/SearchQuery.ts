026d9f87ac26b839807b1c1bd5df982194ed8a16
Rémi Marseault <larsluph@larsluph.dev>
2025-10-26 20:10:33 +0100

    perf(SearchQuery): Group values between double quotes

--- src/composables/SearchQuery.ts
+++ src/composables/SearchQuery.ts
@@ -19,7 +19,7 @@ export function useSearchQuery<T>(
       const regex = new RegExp(query.substring(1, query.length - 2))
       res = searchItems.filter(item => [getter(item)].flat().some(singleItem => regex.test(singleItem ?? '')))
     } else {
-      const tokens = normalize(query).trim().split(/[ ,]/i).filter(Boolean)
+      const tokens = tokenize(query)
       const inclusionTokens = tokens.filter(token => !token.startsWith('-'))
       const exclusionTokens = tokens.filter(token => token.startsWith('-')).map(token => token.slice(1))
       res = searchItems.filter(item => handleIncludeTokens(item, inclusionTokens) && handleExcludeTokens(item, exclusionTokens))
@@ -27,6 +27,43 @@ export function useSearchQuery<T>(
 
     return postProcess ? postProcess(res) : res
   })
+
+  function tokenize(query: string): string[] {
+    const tokens = new Array<string>()
+    let buffer = ''
+    let quoteFlag = false
+
+    function pushBuffer() {
+      tokens.push(buffer)
+      buffer = ''
+    }
+
+    for (const c of normalize(query)) {
+      if (c === '"') {
+        if (quoteFlag) {
+          quoteFlag = false
+          pushBuffer()
+          continue
+        } else {
+          quoteFlag = true
+          continue
+        }
+      } else if (quoteFlag) {
+        // Skip process, append to buffer until quote or end
+      } else if ([' ', ','].includes(c)) {
+        pushBuffer()
+        continue
+      }
+
+      buffer += c
+    }
+
+    if (buffer !== '') {
+      pushBuffer()
+    }
+
+    return tokens
+  }
 
   function handleIncludeTokens(item: T, tokens: string[]) {
     return tokens.every(token => {
