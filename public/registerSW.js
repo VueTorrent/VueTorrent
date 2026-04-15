d82c6f21fc5a4a3437c34ca549af87edb27e091f
Larsluph <github@larsluph.dev>
2026-04-15 13:08:38 +0000

    Deploying to nightly-release from @ VueTorrent/VueTorrent@6fa6dfdec5beeed0f8c375122eded69b842d72dc 🚀

--- public/registerSW.js
+++ public/registerSW.js
@@ -1,1 +1,1 @@
-"serviceWorker"in navigator&&window.addEventListener("load",()=>navigator.serviceWorker.register("sw.js"))
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
